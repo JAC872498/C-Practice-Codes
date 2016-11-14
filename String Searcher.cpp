e2784827dc8a60aee4eb0c1047286689897df078
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-14 11:12:54 -0600

    Update String Searcher.cpp

--- String Searcher.cpp
+++ String Searcher.cpp
@@ -5,45 +5,45 @@ Takes 2 string(a and b)searches for the smaller in the larger string.*/
 using namespace std
 
 int main() {
-	string a,b,newWrd;///a and b are the strings given by user to search/be searched.
-	int tot;//amount of times one string shows up in the other.
+	string a,b,newWrd;///a and b are the strings given by user to search/be searched
+	int tot;//amount of times one string shows up in the other
 	cin>>a>>b;
-	string FileMeasure=a;//turning string a into an array.
+	string FileMeasure=a;//turning string a into an array
 	int TempNumOne=FileMeasure.size();
 	char Aarr[100];
 	for (int a=0;a<=TempNumOne;a++){
     	Aarr[a]=FileMeasure[a];
 	}
-	string FileMeasure2=b;//turning string b into an array.
+	string FileMeasure2=b;//turning string b into an array
 	int TempNumOne2=FileMeasure2.size();
 	char Barr[100];
 	for (int a=0;a<=TempNumOne2;a++){
     	Barr[a]=FileMeasure2[a];
 	}
 	
-	if(a.length()>b.length()){//Searching for b in a
+	if(a.length()>b.length()){//searching for b in a
 		for(int x=0;x<=a.length()-b.length();x++){
-			for(int n=x;n<x+b.length();n++){
+			for(int n=x;n<x+b.length();n++){//getting a chunk of a the length of b
 				newWrd=newWrd+Aarr[n];
 			}
-			if(newWrd==b){
+			if(newWrd==b){//adding to tot if the chunk is equal to b
 				tot=tot+1;
 			}
-			newWrd="";	
+			newWrd="";//reseting the code chunk	
 		}
 		cout<<b<<" is found in "<<a<<" "<<tot<<" time(s)";
-	}else if(b.length()>a.length()){//Searching for a in b
+	}else if(b.length()>a.length()){//searching for a in b
 		for(int x=0;x<=b.length()-a.length();x++){
-			for(int n=x;n<x+a.length();n++){
+			for(int n=x;n<x+a.length();n++){//getting a chunk of b the length of a
 				newWrd=newWrd+Barr[n];
 			}
-			if(newWrd==a){
+			if(newWrd==a){//adding to tot if the chunk if equal to a
 				tot=tot+1;
 			}
-			newWrd="";
+			newWrd="";//reseting the code chunk
 		}
 		cout<<a<<" is found in "<<b<<" "<<tot<<" time(s)";
-	}else{//Both a and b are the same
+	}else{//both a and b are the same
 		if(a==b){
 			cout<<"They're the same string.";
 		}
