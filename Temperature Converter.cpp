8919b16e4ea07229725ac4249c0ff209eb3c87f9
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-16 13:53:03 -0600

    Update Temperature Converter.cpp

--- Temperature Converter.cpp
+++ Temperature Converter.cpp
@@ -1,1 +1,8 @@
-/**/
+/*This takes either celsius, kelvin, fahrenheit and converts it into either of the other two choices.
+The end result is rounded to the hundredths*/
+
+int main(){
+  String choice;
+  cout<<
+  return 0; 
+}
