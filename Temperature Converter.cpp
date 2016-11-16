72669f07bf8661bf6cbaff626c34405dd07e4122
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-16 14:31:28 -0600

    Update Temperature Converter.cpp

--- Temperature Converter.cpp
+++ Temperature Converter.cpp
@@ -2,10 +2,34 @@
 The end result is rounded to the hundredths*/
 
 #include <iostream>
-using namespace std
+using namespace std;
 
 int main(){
-  string choice;
-  cout<<
-  return 0; 
+  int deg;
+  string oldTemp,newTemp;
+  cout<<"Enter the temperature amount(just the number).\n";
+  cin>>deg;
+  cout<<"Enter C, F, or K for the temperature you just put in.\n";
+  cin>>oldTemp;
+  cout<<"Enter C, F, or K for the temperature you want.\n";
+  cin>>newTemp;
+  if(newTemp=="C"||newTemp=="c"){
+    if(){
+      
+    }else if(){
+      
+    }
+  }else if(newTemp=="F"||newTemp=="f"){
+    if(){
+      
+    }else{
+      
+    }
+  }else if(newTemp=="K"||newTemp=="k"){
+    if(){
+      
+    }else{
+      
+    }
+  }
 }
