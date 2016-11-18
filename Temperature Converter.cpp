32b5f3a96fe3e5222c74c372ee3d1d00394a0ced
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-18 10:50:17 -0600

    Update Temperature Converter.cpp

--- Temperature Converter.cpp
+++ Temperature Converter.cpp
@@ -13,23 +13,29 @@ int main(){
   cin>>oldTemp;
   cout<<"Enter C, F, or K for the temperature you want.\n";
   cin>>newTemp;
-  if(newTemp=="C"||newTemp=="c"){
-    if(){
-      
-    }else if(){
-      
+  if(oldTemp=="C"||oldTemp=="c"){//If the original temp is in celcius
+    if(newTemp=="K"||newTemp=="k"){//Celcius --> Kelvin
+      cout<<"C to K";
+    }else if(newTemp=="F"||newTemp=="f"){//Celcius --> Fahrenheit
+      cout<<"C to F";
+    }else{//Neither of the two
+      cout<<"C";
     }
-  }else if(newTemp=="F"||newTemp=="f"){
-    if(){
-      
-    }else{
-      
+  }else if(oldTemp=="F"||oldTemp=="f"){//If the original temp is if fahrenheit
+    if(newTemp=="K"||newTemp=="k"){//Fahrenheit --> Kelvin
+      cout<<"F to K";
+    }else if(newTemp=="C"||newTemp=="c"){//Fahrenheit --> Celcius
+      cout<<"F to C";
+    }else{//Neither of the two
+      cout<<"F";
     }
-  }else if(newTemp=="K"||newTemp=="k"){
-    if(){
-      
-    }else{
-      
+  }else if(oldTemp=="K"||oldTemp=="k"){//If the original temp is in kelvin
+    if(newTemp=="C"||newTemp=="c"){//Kelvin --> Celcius
+      cout<<"K to C";
+    }else if(newTemp=="F"||newTemp=="f"){//Kelvin --> Fahrenheit
+      cout<<"K to F";
+    }else{//Neither of the two
+      cout<<"K";
     }
   }
 }
