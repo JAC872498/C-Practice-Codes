0d1709287202673b9f1bd2627043179e1714512f
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-20 00:17:53 -0600

    Update Temperature Converter.cpp

--- Temperature Converter.cpp
+++ Temperature Converter.cpp
@@ -5,35 +5,51 @@ The end result is rounded to the hundredths*/
 using namespace std;
 
 int main(){
-  double deg;
+  double lowDeg,highDeg,interval;
   string oldTemp,newTemp;
-  cout<<"Enter the temperature amount(just the number).\n";
-  cin>>deg;
+  cout<<"Enter the lowest temperature amount(just the number).\n";
+  cin>>lowDeg;
+  cout<<"Enter the highest temperature amount(also just the number).\n";
+  cin>>highDeg;
+  cout<<"Enter the interval.\n";
+  cin>>interval;
   cout<<"Enter C, F, or K for the temperature you just put in.\n";
   cin>>oldTemp;
   cout<<"Enter C, F, or K for the temperature you want.\n";
   cin>>newTemp;
   if(oldTemp=="C"||oldTemp=="c"){//If the original temp is in celsius
-    if(newTemp=="K"||newTemp=="k"){//Celsius --> Kelvin
-      cout<<deg+273.15;
+  	  if(newTemp=="K"||newTemp=="k"){//Celsius --> Kelvin
+	  for(double x=lowDeg+273.15;x<=highDeg+273.15;x+=interval){
+		cout<<x<<"\n";
+	  }
     }else if(newTemp=="F"||newTemp=="f"){//Celsius --> Fahrenheit
-      cout<<deg*1.8+32;
+      for(double x=lowDeg*1.8+32;x<=highDeg*1.8+32;x+=interval){
+		cout<<x<<"\n";
+	  }
     }else{//Neither of the two
       cout<<"Only enter F or K";
     }
   }else if(oldTemp=="F"||oldTemp=="f"){//If the original temp is if fahrenheit
-    if(newTemp=="K"||newTemp=="k"){//Fahrenheit --> Kelvin
-      cout<<(deg+459.67)*(5/9);
+  	  if(newTemp=="K"||newTemp=="k"){//Fahrenheit --> Kelvin
+  	  for(double x=(lowDeg+459.67)*0.556;x<=(highDeg+459.67)*0.556;x+=interval){
+		cout<<x<<"\n";//I use .556 instead of 5/9 because the code refuses fractions
+	  }
     }else if(newTemp=="C"||newTemp=="c"){//Fahrenheit --> Celsius
-      cout<<(deg-32)/1.8;
+      for(double x=(lowDeg-32)/1.8;x<=(highDeg-32)/1.8;x+=interval){
+		cout<<x<<"\n";
+	  }
     }else{//Neither of the two
       cout<<"Only enter C or K";
     }
   }else if(oldTemp=="K"||oldTemp=="k"){//If the original temp is in kelvin
     if(newTemp=="C"||newTemp=="c"){//Kelvin --> Celsius
-      cout<<deg-273.15;
+  	  for(double x=lowDeg-273.15;x<=highDeg-273.15;x+=interval){
+		cout<<x<<"\n";
+	  }
     }else if(newTemp=="F"||newTemp=="f"){//Kelvin --> Fahrenheit
-      cout<<deg*1.8-459.67;
+      for(double x=lowDeg*1.8-459.67;x<=highDeg*1.8-459.67;x+=interval){
+		cout<<x<<"\n";
+	  }
     }else{//Neither of the two
       cout<<"Only enter C or F";
     }
