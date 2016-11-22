f36200c6091e0901b71c5e3331273e6d4d4e674e
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-21 22:36:53 -0600

    Update Dungeon Crawler.cpp

--- Dungeon Crawler.cpp
+++ Dungeon Crawler.cpp
@@ -3,23 +3,32 @@
 
 using namespace std;
 
-int main(){
-	int xLen=6,yLen=6;//This is the range of the 'map'
+int main(){//Have enemys move around and have 'map' area changable
+	int xLen,yLen;//This is the range of the 'map'
 	int player[]={3,3};//Represented as 'P', this is who the player controls
-	int goal[]={6,6};//Represented as 'X', this is where the player wants to get to.
+	int goal[]={6,6};//Represented as 'X', this is where the player wants to get to
+	int enemy[]={1,1,2,2};//Represented as 'E', these are 2 enemys that kill the player
 	string move;
+	cout<<"Enter the graph x amount.\n";//Getting the area of the 'map'
+	cin>>xLen;
+	cout<<"Enter the graph y amount.\n";
+	cin>>yLen;
 	while(true){
 		if(player[0]==goal[0]&&player[1]==goal[1]){//If the player reaches 'X'
+			cout<<"You Win!";
 			break;
-		}else if(player[0]>6||player[1]>6||player[0]<=0||player[1]<=0){//If the player goes out of bounds
+		}else if((player[0]==enemy[0]&&player[1]==enemy[1])||(player[0]==enemy[2]&&player[1]==enemy[3])){//If the player hits "E"
+			cout<<"You Lose!";
 			break;
 		}
 		for(int y=1;y<=yLen;y++){//This chunk is for displaying the board
 			for(int x=1;x<=xLen;x++){
-				if(player[0]==x && player[1]==y){
+				if(player[0]==x&&player[1]==y){
 				   	cout<<"P";
-				}else if(goal[0]==x && goal[1]==y){
+				}else if(goal[0]==x&&goal[1]==y){
 					cout<<"X";
+				}else if((enemy[0]==x&&enemy[1]==y)||(enemy[2]==x&&enemy[3]==y)){
+					cout<<"E";
 				}else{
 					cout<<"*";
 				}
@@ -27,13 +36,13 @@ int main(){
 			cout<<"\n";
 		}
 		cin>>move;//This is chunk is for movement
-		if(move=="left"){
+		if(move=="left"&&player[0]!=1){
 			player[0]=player[0]-1;
-		}else if(move=="right"){
+		}else if(move=="right"&&player[0]!=6){
 			player[0]=player[0]+1;
-		}else if(move=="up"){
+		}else if(move=="up"&&player[1]!=1){
 			player[1]=player[1]-1;
-		}else if(move=="down"){
+		}else if(move=="down"&&player[1]!=6){
 			player[1]=player[1]+1;
 		}
 	}
