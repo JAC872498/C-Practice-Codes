e84019c478d4fb88807b837c3a8bdd7a08293d92
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-22 23:43:46 -0600

    Update Dungeon Crawler.cpp

--- Dungeon Crawler.cpp
+++ Dungeon Crawler.cpp
@@ -37,7 +37,7 @@ int main(){
 			}
 			cout<<"\n";
 		}
-		cin>>move;//This is chunk is for movement
+		cin>>move;//This is chunk is for movement the player's move
 		if(move=="left"&&player[0]!=1){
 			player[0]=player[0]-1;
 		}else if(move=="right"&&player[0]!=6){
