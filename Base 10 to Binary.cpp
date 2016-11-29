23d7d7e75eb9a20acecb8aa780e76214a245d20f
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-11-29 07:36:51 -0600

    Update Base 10 to Binary.cpp

--- Base 10 to Binary.cpp
+++ Base 10 to Binary.cpp
@@ -1,25 +1,40 @@
-/**/
-
 #include <iostream>
 #include <string> 
 using namespace std;
 
 int binary(int x){
 	int	bin=0;
-	string s="";
-	for(int n=0;n<x;n++){
+	string wrd="";
+	string newWrd="";
+	for(int n=0;n<x;n++){//Creating the number
 		bin++;
-		s=to_string(bin);
-		cout<<s[1];/*USE THIS*/
-		if(bin%2==0 && bin%5!=0){
-			bin=bin+8;
+		wrd=to_string(bin);
+		for(int a=0;a<wrd.length();a++){//Going through each number 
+			if((wrd[a]-'0')==2){//If any number is a 2
+				//Turn the 2 at s[a] into a 10
+				if(a==0){//2 at the very front
+					for(int str=1;str<wrd.length();str++){
+						newWrd[str-1]=wrd[str];
+					}
+					newWrd="10"+newWrd;
+					bin=newWrd to int;
+				}else if(a-1==s.length()){//2 at the very back
+					for(int str=0;str<wrd.length()-1;str++){
+						newWrd[str]=wrd[str];
+					}
+					newWrd=newWrd="0";
+					newWrd[newWrd.length()-2]=((newWrd[newWrd.length()-2]-'0')+1) to string;
+					bin=;
+				}else{//2 in the middle
+					wrd=;
+					bin=;
+				}
+			}	
 		}
 	}
 	return bin;
 }
 
-int main() {
-	//cout<<binary(4);
-	
+int main(){
 	return 0;
 }
