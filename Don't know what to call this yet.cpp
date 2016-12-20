69f8db4f50ce5c1dffce20564682f98eff884fcf
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-12-19 21:07:15 -0600

    Update Don't know what to call this yet.cpp

--- Don't know what to call this yet.cpp
+++ Don't know what to call this yet.cpp
@@ -1,4 +1,4 @@
-#include <iostream>//This is like dungeon crawler, but with no enemies(yet) and you have to search for the exit
+#include <iostream>
 #include <stdlib.h>
 
 using namespace std;
@@ -6,26 +6,40 @@ using namespace std;
 bool stuff(int arr[],int x, int y){
 	int xPlayer=arr[0];
 	int yPlayer=arr[1];
-	if(abs(x-xPlayer)==1 || abs(y-yPlayer)==1){//WORK ON THIS
+	if((abs(x-xPlayer)==1 && abs(y-yPlayer)<=1) || (abs(y-yPlayer)==1 && abs(x-xPlayer)<=1)){
 		return true;
-	}return false;
+	}return false;//Have this permanently mark spaces as */E/P instead of ?
 }
 
 int main(){
 	int player[]={0, 0};
 	int xLen=10;
 	int yLen=10;
-	for(int x=0;x<xLen;x++){
+	string move;
+	while(true){
 		for(int y=0;y<yLen;y++){
-			if(player[0]==x && player[1]==y){
-				cout<<"P";
-			}else if(stuff(player,x,y)){
-				cout<<"*";
-			}else{
-				cout<<"?";
-			}
+			for(int x=0;x<xLen;x++){
+				   	if(player[0]==x && player[1]==y){
+				   	cout<<"P";
+				   	}else if(stuff(player,x,y)){
+					  		cout<<"*";
+					}else{//Add E(end)
+			 			cout<<"?";
+				  	}
+		  		}
+				cout<<"\n";
+	   		}
+		//break once player reaches E
+		cin>>move;//This is chunk is for movement
+		if(move=="left"&&player[0]!=1){//Player moving left
+			player[0]=player[0]-1;
+		}else if(move=="right"&&player[0]!=xLen){//Player moving right
+			player[0]=player[0]+1;
+		}else if(move=="up"&&player[1]!=1){//Player moving up
+			player[1]=player[1]-1;
+		}else if(move=="down"&&player[1]!=yLen){//Player moving down
+			player[1]=player[1]+1;
 		}
-		cout<<"\n";
 	}
 	return 0;
 }
