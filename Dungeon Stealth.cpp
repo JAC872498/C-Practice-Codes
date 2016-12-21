9680ead75342322a571836aff746ceb9e0532bff
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-12-20 20:21:08 -0600

    Update and rename Don't know what to call this yet.cpp to Dungeon Stealth.cpp

--- Dungeon Stealth.cpp
+++ Dungeon Stealth.cpp
@@ -10,19 +10,27 @@ bool stuff(int arr[],int x, int y){
 	int yPlayer=arr[1];
 	if((abs(x-xPlayer)<=2 && abs(y-yPlayer)<=2) || (abs(y-yPlayer)<=2 && abs(x-xPlayer)<=2)){
 		return true;
-	}return false;//Have this permanently mark spaces as */E/P instead of ?
+	}return false;
 }
 
 int main(){
 	srand(time(NULL));
 	int player[]={0, 0};
-	int goal[]={rand()%11,rand()%11};
 	int xLen=10;
 	int yLen=10;
+	cout<<"Enter the x length of the grid.\n";
+	cin>>xLen;
+	cout<<"Enter the y length of the grid.\n";
+	cin>>yLen;
 	string move;
+	int goal[]={rand()%xLen,rand()%yLen};
+	int enemy[]={rand()%xLen,rand()%yLen};
 	while(true){
 		if(player[0]==goal[0]&&player[1]==goal[1]){
 			cout<<"You win!";
+			break;
+		}else if(player[0]==enemy[0]&&player[1]==enemy[1]){
+			cout<<"You lose!";
 			break;
 		}
 		for(int y=0;y<yLen;y++){
@@ -32,6 +40,8 @@ int main(){
 					}else if(stuff(player,x,y)){
 				   		if(goal[0]==x && goal[1]==y){
 					  		cout<<"X";
+						}else if(enemy[0]==x && enemy[1]==y){
+							cout<<"E";
 						}else{
 							cout<<"*";
 						}
@@ -50,6 +60,16 @@ int main(){
 			player[1]=player[1]-1;
 		}else if(move=="down"&&player[1]!=9){//Player moving down
 			player[1]=player[1]+1;
+		}
+		int enemy1Movement=rand()%4+1;
+		if(enemy1Movement==1&&enemy[0]!=0){//Enemy 1 moving left
+			enemy[0]=enemy[0]-1;
+		}else if(enemy1Movement==2&&enemy[1]!=0){//Enemy 1 moving up
+			enemy[1]=enemy[1]-1;
+		}else if(enemy1Movement==3&&enemy[0]!=xLen-1){//Enemy 1 moding right
+			enemy[0]=enemy[0]+1;
+		}else if(enemy1Movement==4&&enemy[1]!=yLen-1){//Enemy 1 moving down
+			enemy[1]=enemy[1]+1;
 		}
 	}
 	return 0;
