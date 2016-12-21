78f663ae71587a1fcd69816a515a31437f6ef679
Jordan Camilletti <jordan.camilletti@gmail.com>
2016-12-21 13:12:10 -0600

    Update Dungeon Stealth.cpp

--- Dungeon Stealth.cpp
+++ Dungeon Stealth.cpp
@@ -5,13 +5,15 @@
 
 using namespace std;
 
-bool stuff(int arr[],int x, int y){
+bool canSee(int arr[],int x, int y){
 	int xPlayer=arr[0];
 	int yPlayer=arr[1];
 	if((abs(x-xPlayer)<=2 && abs(y-yPlayer)<=2) || (abs(y-yPlayer)<=2 && abs(x-xPlayer)<=2)){
 		return true;
 	}return false;
 }
+
+
 
 int main(){
 	srand(time(NULL));
@@ -37,7 +39,7 @@ int main(){
 			for(int x=0;x<xLen;x++){
 				   	if(player[0]==x && player[1]==y){
 				   		cout<<"P";
-					}else if(stuff(player,x,y)){
+					}else if(canSee(player,x,y)){
 				   		if(goal[0]==x && goal[1]==y){
 					  		cout<<"X";
 						}else if(enemy[0]==x && enemy[1]==y){
