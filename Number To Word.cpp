794b69e9a33275b5bdf57764182731950885b412
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-02 15:04:47 -0600

    Update Number To Word.cpp

--- Number To Word.cpp
+++ Number To Word.cpp
@@ -1,1 +1,30 @@
-/**/
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout<<"Enter your number.\n";
+	int starting=69;
+	cin>>starting;
+	if(starting==0){
+		cout>>"zero";
+	}else if(starting==1){
+		cout>>"one";
+	}else if(starting==2){
+		cout>>"two";
+	}else if(starting==3){
+		cout>>"three";
+	}else if(starting==4){
+		cout>>"four";
+	}else if(starting==5){
+		
+	}else if(starting==6){
+		
+	}else if(starting==7){
+		
+	}else if(starting==8){
+		
+	}else if(starting==9){
+		
+	}
+	return 0;
+}
