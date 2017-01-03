60aacd7f347e4138106e7a9557194eee6eafab93
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-03 10:01:42 -0600

    Update Number To Word.cpp

--- Number To Word.cpp
+++ Number To Word.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-string num(int starting){
-	string ones[]={"zero","one","two","three","four","five","six","seven","eight","nine",};
+int num(int starting){
+	/*string ones[]={"zero","one","two","three","four","five","six","seven","eight","nine",};
 	string wrd="";
-	return ones[starting];
+	return num[0];*/
+	int arr[to_string(starting).length()];
+	for (int i=to_string(starting).length()-1;i>=0;i--) {
+    	arr[i] = starting % 10;
+    	starting /= 10;
+	}
+	return arr[0];
 }
 
 int main() {
-	int starting=6;
+	/*int starting=12345;
 	cout<<"Enter your number.\n";
-	cin>>starting;
-	cout<<num(starting);
+	cin>>starting;*/
+	cout<<num(1234);
 	return 0;
 }
