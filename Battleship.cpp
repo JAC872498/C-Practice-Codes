4ee1f7de8d6658ac22d6660e39a5b486edc69e44
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-11 11:29:39 -0600

    Create Battleship.cpp

---
+++ Battleship.cpp
@@ -0,0 +1,9 @@
+/*This basicly recreates the game of battle ship with S being ships, O being water on your board. and M being miss and E being hit on enemy's board.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+   return 0;
+}
