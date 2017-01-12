93f2c7dfcd3d2dc676e64433cbfe58169f5f54b0
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-12 11:39:32 -0600

    Update Battleship.cpp

--- Battleship.cpp
+++ Battleship.cpp
@@ -1,24 +1,39 @@
 /*This basicly recreates the game of battle ship with S being ships, O being water on your board. and M being miss and E being hit on enemy's board.*/
 
-// Example program
 #include <iostream>
 #include <string>
 using namespace std;
-
-int shipThere(int x, int y, int ship1[], int ship2[], int ship3[], int ship4[]){
-    if((ship1[0]==x && ship1[1]==y)||(ship2[0]==x && ship2[1]==y)||()||())
-}
 
 int main(){
     int xLen=10;
     int yLen=10;
     int ship1[]={0,0};
-    int ship2[]={1,1};
-    int ship3[]={2,2};
-    int ship4[]={3,3};
+    int ship2[]={0,0};
+    int ship3[]={0,0};
+    int ship4[]={0,0};
+    cout<<"Enter the coordonates of ship 1.\n";
+    cin>>ship1[0]>>ship1[1];
+    ship1[0]--;
+    ship1[1]--;
+    cout<<"Enter the coordonates of ship 2.\n";
+    cin>>ship2[0]>>ship2[1];
+    ship2[0]--;
+    ship2[1]--;
+    cout<<"Enter the coordonates of ship 3.\n";
+    cin>>ship3[0]>>ship3[1];
+    ship3[0]--;
+    ship3[1]--;
+    cout<<"Enter the coordonates of ship 4.\n";
+    cin>>ship4[0]>>ship4[1];
+    ship4[0]--;
+    ship4[1]--;
     for(int y=0;y<yLen;y++){
         for(int x=0;x<xLen;x++){
-            cout<<"O ";
+            if((ship1[0]==x && ship1[1]==y)||(ship2[0]==x && ship2[1]==y)||(ship3[0]==x && ship3[1]==y)||(ship4[0]==x && ship4[0]==y)){
+                cout<<"S ";
+            }else{
+                cout<<"O ";
+            }
         }
         cout<<"\n";
     }
