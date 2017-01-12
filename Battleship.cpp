1d982cda2a9cd4bfadabb9821a4bf80b23581b66
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-12 12:16:07 -0600

    Update Battleship.cpp

--- Battleship.cpp
+++ Battleship.cpp
@@ -4,6 +4,14 @@
 #include <string>
 using namespace std;
 
+string hit(int guess[], int ship[],int x, int y, bool hit){
+    if((guess[0]-1==x && guess[1]-1==y)||hit){
+        return("E ");
+    }else{
+        return("S ");
+    }
+}
+
 int main(){
     int xLen=10;
     int yLen=10;
@@ -11,30 +19,54 @@ int main(){
     int ship2[]={0,0};
     int ship3[]={0,0};
     int ship4[]={0,0};
-    cout<<"Enter the coordonates of ship 1.\n";
+    int guess[]={0,0};
+    bool ship1Hit=false;
+    bool ship2Hit=false;
+    bool ship3Hit=false;
+    bool ship4Hit=false;
+    cout<<"Enter the coordinates of ship 1.\n";
     cin>>ship1[0]>>ship1[1];
     ship1[0]--;
     ship1[1]--;
-    cout<<"Enter the coordonates of ship 2.\n";
+    cout<<"Enter the coordinates of ship 2.\n";
     cin>>ship2[0]>>ship2[1];
     ship2[0]--;
     ship2[1]--;
-    cout<<"Enter the coordonates of ship 3.\n";
+    cout<<"Enter the coordinates of ship 3.\n";
     cin>>ship3[0]>>ship3[1];
     ship3[0]--;
     ship3[1]--;
-    cout<<"Enter the coordonates of ship 4.\n";
+    cout<<"Enter the coordinates of ship 4.\n";
     cin>>ship4[0]>>ship4[1];
     ship4[0]--;
     ship4[1]--;
-    for(int y=0;y<yLen;y++){
-        for(int x=0;x<xLen;x++){
-            if((ship1[0]==x && ship1[1]==y)||(ship2[0]==x && ship2[1]==y)||(ship3[0]==x && ship3[1]==y)||(ship4[0]==x && ship4[0]==y)){
-                cout<<"S ";
-            }else{
-                cout<<"O ";
+    for(int n=0;n<5;n++){
+        cout<<"Enter firing coordinates.\n";
+        cin>>guess[0]>>guess[1];
+        for(int y=0;y<yLen;y++){
+            for(int x=0;x<xLen;x++){
+                if(ship1[0]==x && ship1[1]==y){
+                    cout<<hit(guess, ship1, x, y, ship1Hit);
+                    ship1Hit=true;
+                }else if(ship2[0]==x && ship2[1]==y){
+                    cout<<hit(guess, ship2, x, y, ship2Hit); 
+                    ship2Hit=true;
+                }else if(ship3[0]==x && ship3[1]==y){
+                    cout<<hit(guess, ship3, x, y, ship3Hit);
+                    ship3Hit=true;
+                }else if(ship4[0]==x && ship4[0]==y){
+                    cout<<hit(guess, ship4, x, y, ship4Hit);
+                    ship4Hit=true;
+                }else{
+                    if(guess[0]-1==x && guess[1]-1==y){
+                        cout<<"M ";
+                    }else{
+                        cout<<"O ";
+                    }
+                }
             }
+            cout<<"\n";
         }
-        cout<<"\n";
     }
+    return (0);
 }
