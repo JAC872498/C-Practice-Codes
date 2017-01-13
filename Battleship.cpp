394225157f9a61c86f9c0deb9aa579c4816b31ca
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-13 08:30:38 -0600

    Update Battleship.cpp

--- Battleship.cpp
+++ Battleship.cpp
@@ -4,6 +4,8 @@
 #include <string>
 using namespace std;
 
+
+
 int main(){
     string myGrid[10][10]={
         {"O","O","O","O","O","O","O","O","O","O"},
@@ -24,29 +26,20 @@ int main(){
     int guess[]={0,0};
     cout<<"Enter the coordinates of ship 1.\n";
     cin>>ship1[0]>>ship1[1];
-    ship1[0]--;
-    ship1[1]--;
-    myGrid[ship1[0]][ship1[1]]="1";
+    myGrid[ship1[0]-1][ship1[1]-1]="1";
     cout<<"Enter the coordinates of ship 2.\n";
     cin>>ship2[0]>>ship2[1];
-    ship2[0]--;
-    ship2[1]--;
-    myGrid[ship2[0]][ship2[1]]="2";
+    myGrid[ship2[0]-1][ship2[1]-1]="2";
     cout<<"Enter the coordinates of ship 3.\n";
     cin>>ship3[0]>>ship3[1];
-    ship3[0]--;
-    ship3[1]--;
-    myGrid[ship3[0]][ship3[1]]="3";
+    myGrid[ship3[0]-1][ship3[1]-1]="3";
     cout<<"Enter the coordinates of ship 4.\n";
     cin>>ship4[0]>>ship4[1];
-    ship4[0]--;
-    ship4[1]--;
-    myGrid[ship4[0]][ship4[1]]="4";
+    myGrid[ship4[0]-1][ship4[1]-1]="4";
     for(int n=0;n<5;n++){
         cout<<"Enter firing coordinates.\n";
         cin>>guess[0]>>guess[1];
-        guess[0]--;
-        guess[1]--;
+        myGrid[guess[0]-1][guess[1]-1]="H";
         for(int y=0;y<10;y++){
             for(int x=0;x<10;x++){
                 cout<<myGrid[x][y];
