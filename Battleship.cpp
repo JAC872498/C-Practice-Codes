bc4bff730c16187d177eff2ecab366519940ea4c
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-13 12:06:58 -0600

    Update Battleship.cpp

--- Battleship.cpp
+++ Battleship.cpp
@@ -18,9 +18,16 @@ int main(){
         {"O","O","O","O","O","O","O","O"},
         {"O","O","O","O","O","O","O","O"}, 
     };
-    /*string enemyGrid[8][8]={
-        
-    }*/
+    string enemyGrid[8][8]={
+        {"?","?","?","?","?","?","?","?"},
+        {"?","?","?","?","?","?","?","?"},
+        {"?","?","?","?","?","?","?","?"},
+        {"?","?","?","?","?","?","?","?"},
+        {"?","?","?","?","?","?","?","?"},
+        {"?","?","?","?","?","?","?","?"},
+        {"?","?","?","?","?","?","?","?"},
+        {"?","?","?","?","?","?","?","?"},
+    };
     bool ship1Hit=false;
     bool ship2Hit=false;
     bool ship3Hit=false;
@@ -50,7 +57,7 @@ int main(){
     myGrid[ship5[0]-1][ship5[1]-1]="S";
     while(turns!=0&&(!ship1Hit || !ship2Hit || !ship3Hit || !ship4Hit || !ship5Hit)){
         turns--;
-        cout<<"Turn "<<8-turns<<"\n";
+        cout<<"Turn "<<8-turns<<".\n";
         guess[0]=rand()%8;
         guess[1]=rand()%8;
         if(guess[0]==ship1[0]-1&&guess[1]==ship1[1]-1){
@@ -76,9 +83,17 @@ int main(){
         }else{
             myGrid[guess[0]-1][guess[1]-1]="M";
         }
+        cout<<"Your grid:\n";
         for(int y=0;y<8;y++){
             for(int x=0;x<8;x++){
                 cout<<myGrid[x][y];
+            }
+            cout<<"\n";
+        }
+        cout<<"Enemy's grid:\n";
+        for(int y=0;y<8;y++){
+            for(int x=0;x<8;x++){
+                cout<<enemyGrid[x][y];
             }
             cout<<"\n";
         }
