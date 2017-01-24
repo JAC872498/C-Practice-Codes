5d6ad319b7e5e5dfe71a36c61e41831b978ba3e0
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-24 11:23:07 -0600

    Update Binary Base Ball.cpp

--- Binary Base Ball.cpp
+++ Binary Base Ball.cpp
@@ -10,7 +10,7 @@ int main(){
   int myScore=0;
   int enemyScore=0;
   int outs=0;
-  int hit=rand()%100;//50-100=out, 26-
+  int hit=rand()%100;//50-100=out, 50-30=single, 30-15=double, 15-5=triple, 5-0=home run
   srand(time(NULL));
   bool playing=false;//true if you're batting, false if enemy is batting
   if(playing){
@@ -18,8 +18,14 @@ int main(){
           hit=rand()%100;
           if(hit>=50){
             outs+=1;
-          }else{
-            cout<<"You got a hit\n";  
+          }else if(hit>=30){
+            //single
+          }else if(hit>=15){
+            //double
+          }else if(hit>=5){
+            //triple  
+          }else if(hit>=0){
+            //home run
           }
       }
   }else{
@@ -27,8 +33,14 @@ int main(){
           hit=rand()%100;
           if(hit>=50){
             outs+=1;
-          }else{
-            cout<<"Enemy got a hit\n";  
+          }else if(hit>=30){
+            //single  
+          }else if(hit>=15){
+            //double
+          }else if(hit>=5){
+            //triple  
+          }else if(hit>=0){
+            //home run  
           }
       }
   }
