2e5a756c995f295009e8b6115ae23d43a1501d49
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-24 10:54:57 -0600

    Update Binary Base Ball.cpp

--- Binary Base Ball.cpp
+++ Binary Base Ball.cpp
@@ -4,27 +4,31 @@
 using namespace std;
 
 int main(){
-  bool onFirst=false;
-  bool onSecond=false;
-  bool onThird=false;
+  int onFirst=0;
+  int onSecond=0;
+  int onThird=0;
   int myScore=0;
   int enemyScore=0;
   int outs=0;
-  int hit=rand()%5;
+  int hit=rand()%100;//50-100=out, 26-
   srand(time(NULL));
   bool playing=false;//true if you're batting, false if enemy is batting
   if(playing){
       while(outs!=3){
-          hit=rand()%5;
-          if(hit==0){
+          hit=rand()%100;
+          if(hit>=50){
             outs+=1;
+          }else{
+            cout<<"You got a hit\n";  
           }
       }
   }else{
       while(outs!=3){
-          hit=rand()%5;
-          if(hit==0){
+          hit=rand()%100;
+          if(hit>=50){
             outs+=1;
+          }else{
+            cout<<"Enemy got a hit\n";  
           }
       }
   }
