f89b38da7432c10eab1298c917c88fb66f9f53ea
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-31 16:54:03 -0600

    Update Stone Skipping.cpp

--- Stone Skipping.cpp
+++ Stone Skipping.cpp
@@ -1,17 +1,7 @@
 #include <iostream>
 using namespace std;
 
-bool full(double arr[]){//true if full of full meter jumps
-	for(int x=0;x<sizeof(arr)/sizeof(arr[0]);x++){
-		if(arr[x]!=1){
-			return false;
-		}
-	}
-	return true;
-}
-
 int main(){
-    cout<<"x";
 	int river=2;//river length
 	int rocks=(river*2)+1;
 	double jumps[river*2];
@@ -22,27 +12,33 @@ int main(){
 	for(int x=0;x<river*2;x++){//starting off with all .5 meter jumps
 		jumps[x]=0.5;
 	}
-	while(true){
-		if(full(jumps)){//stops when full of 1 meter jumps
-			goto stop;
-		}else{//replaces .5 meter jumps with 1 meter jumps
+	for(int x=0;x<sizeof(jumps)/sizeof(jumps[0]);x++){
+		if(x=0){
+			jumps[x]=1;
+			jumps[sizeof(jumps)/sizeof(jumps[0])-x-1]=0;
+		}else{
+			jumps[x]=1;
+			jumps[x-1]=0.5;
+			jumps[sizeof(jumps)/sizeof(jumps[0])-x-1]=0;
+		}
+		for(int y=x+1;y<y+1;y++){
 			count+=1;
-			currentJump=jumps[jumpNum];
-			for(int x=0;x<sizeof(jumps)/sizeof(jumps[0]);x++){
-				for(int y=x;y<sizeof(jumps)/sizeof(jumps[0]);y++){
-					count+=1;
-					if(y==0||y+1==x){
-						jumps[y]=1;
-					}else{
-						jumps[y]=1;
-						jumps[y-1]=0;
-					}
-					jumps[x]=1;
-				}
+			if(jumps[y]==0){
+				break;
 			}
 		}
 	}
-	stop:
+		for(int y=x;y<jumps.length;y++){
+			count+=1;
+			if(y==0||y+1==x){
+				jumps[y]=1;
+			}else{
+				jumps[y]=1;
+				jumps[y-1]=0;
+			}
+			jumps[x]=1;
+		}
+	}*/
 	cout<<count;
 	return 0;
 }
