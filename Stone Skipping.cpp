33ad6997b0245436e0b7504167df8e94d7ed8000
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-31 13:26:04 -0600

    Update Stone Skipping.cpp

--- Stone Skipping.cpp
+++ Stone Skipping.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
 
+bool full(double e[]){//true if full of full meter jumps
+
+}
+
 int main(){
-	int river=2;
+	double river=2;
 	int rocks=(river*2)+1;
-	int jumps[4]={0,1,2,3};
+	double jumps[river*2];
+	double total=0;
+	for(int x=0;x<river*2;x++){
+		jumps[x]=0.5;
+	}
+	while(true){
+		if(full(jumps[])){
+			goto stop;
+		}
+	}
+	int count=1;
+	stop:
+	cout<<count;
 	return 0;
 }
