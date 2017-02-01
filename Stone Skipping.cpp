2490c9102951d23f9cf75273f4b371e86d49e5d8
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-01-31 19:53:38 -0600

    Update Stone Skipping.cpp

--- Stone Skipping.cpp
+++ Stone Skipping.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-using namespace std;
+#include <vector>
 
-bool is5(double arr[]){
-	for(int x=0;x<(sizeof(arr)/sizeof(arr[0]));x++){//DOSE NOT WORK
-		if(arr[x]!=0.5){
+
+bool is5(std::vector<double> arr){
+	for(int x=0;x<=arr.size();x++){
+		if(arr[x]==0.5){
 			return true;
 		}
 	}
@@ -13,33 +14,38 @@ bool is5(double arr[]){
 int main(){//for(int z=0;z<(sizeof(jumps)/sizeof(jumps[0]));z++){cout<<jumps[z]<<"\n";}cout<<"\n";
 	int river=2;//river length
 	int rocks=(river*2)+1;
-	double jumps[river*2];
+	std::vector<double> jumps;
 	double total=0;
 	int curr=0;
 	int jumpNum=0;
 	int count=1;
 	for(int x=0;x<river*2;x++){//starting off with all .5 meter jumps
-		jumps[x]=0.5;
+		jumps.assign(x,0.5);
 	}
-	for(int z=0;z<(sizeof(jumps)/sizeof(jumps[0]));z++){cout<<jumps[z]<<"\n";}cout<<"\n";
+	jumps[jumps.size()]=0.5;
+	for(int z=0;z<=jumps.size();z++){std::cout<<jumps[z]<<"\n";}std::cout<<"\n";
 	while(true){
-		for(int x=curr;x<(sizeof(jumps)/sizeof(jumps[0])-1);x++){
-			count++;
+		for(int x=curr;x<=jumps.size();x++){
+			total=0;
+			for(int z=0;z<=jumps.size();z++){total+=jumps[z];}
+			if(total==river){	
+				count++;
+			}
 			if(x==curr){
 				jumps[curr]=1;
-				jumps[(sizeof(jumps)/sizeof(jumps[0]))-x-1]=0;
-				for(int z=0;z<(sizeof(jumps)/sizeof(jumps[0]));z++){cout<<jumps[z]<<"\n";}cout<<"\n";
+				jumps[jumps.size()-x]=0;
+				for(int z=0;z<=jumps.size();z++){std::cout<<jumps[z]<<"\n";}std::cout<<"\n";
 			}else{
 	 			jumps[x]=1;
 			   	jumps[x-1]=0.5;
-			   	for(int z=0;z<(sizeof(jumps)/sizeof(jumps[0]));z++){cout<<jumps[z]<<"\n";}cout<<"\n";
+			   	for(int z=0;z<=jumps.size();z++){std::cout<<jumps[z]<<"\n";}std::cout<<"\n";
 			}
 		}
 		jumps[curr]=1;
 		curr++;
-		if(!is5(jumps)) goto stop;//DOSE NOT WORK 
+		if(!is5(jumps)) goto stop;
 	}
 	stop:
-	cout<<count;
+	std::cout<<count;
 	return 0;
 }
