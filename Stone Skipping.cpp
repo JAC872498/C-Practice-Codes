dcca8ff804bfc99569017cf8c449a6d02db4de73
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-02-01 15:55:01 -0600

    Update Stone Skipping.cpp

--- Stone Skipping.cpp
+++ Stone Skipping.cpp
@@ -1,27 +1,13 @@
 #include <iostream>
 
 int power(int number){
-	int z=0;
-	for(int x=0;x<=number;x++) z+=x;
+	int z=1;
+	for(int x=1;x<=(2*number)-1;x++){ z+=x; }
 	return z;
 }
 
 int main(){
-	int total=2;
-	int river=2;
-	for(int x=0;x<=river;x++){
-		if(x==0||x==river){
-			total+=1;
-		}else if(x==1){
-		    total+=((2*number)-1);
-		}else if(x==2){
-		    total+=(((2*(river-1))-1)*river);//TODO: USE REOCURISON STUFF ON PAPER
-		}else if(x==3){			 //TODO EXPLAINED: find out function using paper stuff and keep going down function untill you reach all 0.5
-		    
-		}else if(x==4){
-		    
-		}
-	}
-	//std::cout<<total;
+    int river=3;
+    std::cout<<power(river);
 	return 0;
 }
