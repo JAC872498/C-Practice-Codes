5079e22df07c4e136843747a7ec31587993908c5
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-02-01 14:37:20 -0600

    Update Stone Skipping.cpp

--- Stone Skipping.cpp
+++ Stone Skipping.cpp
@@ -13,7 +13,7 @@ int main(){
 		}else if(x==1){
 		    total+=((2*river)-1);
 		}else if(x==2){
-		    total+=((river*river)-river);//TODO: USE REOCURISON STUFF ON PAPER
+		    total+=(((2*(river-1))-1)*river);//TODO: USE REOCURISON STUFF ON PAPER
 		}else if(x==3){			 //TODO EXPLAINED: find out function using paper stuff and keep going down function untill you reach all 0.5
 		    
 		}else if(x==4){
