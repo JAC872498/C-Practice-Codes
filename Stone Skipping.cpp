f287e846e270fd1367b087712ce250c2cb1f9807
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-02-01 18:17:18 -0600

    Update Stone Skipping.cpp

--- Stone Skipping.cpp
+++ Stone Skipping.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
-#include <cmath>
 
 int power(int number){
 	int z=(2*number)+1;
-	for(int x=(2*number)-3;x>0;x--){ z+=x; }
+	for(int x=(2*number)-3;x>1;x-=2){
+		if(x==5) z+=4;
+		for(int y=x;y>0;y--){ z+=y; }
+	}
 	return z;
 }
 
 int main(){
-    int river=2;
-    std::cout<<power(river);
+	int number=4;
+	std::cout<<power(number);
 	return 0;
 }
