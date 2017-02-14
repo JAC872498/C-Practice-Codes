b8d2883359fda2835051155274483944c826fa90
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-02-14 15:00:12 -0600

    Update Graduation.cpp

--- Graduation.cpp
+++ Graduation.cpp
@@ -3,7 +3,8 @@
 class Bunny{
     bool sex=rand()%2==0;//0\true==female; 1\false==male
     bool rad=rand()%50==0;//0\true==rad; 1-49\false=not rad
-    int age=123;
+    int age=1;
+    int seed = static_cast<int>(time(0));
     std::string colors[4]={"White","Brown","Black","Spotted"};
     std::string names[10]={"Bob","Crigg","Doorknob","Oil","Slam","Skellet0n","Jew","Emo Chick","T3mmi","3l1t3 hax0r"};
     std::string color=colors[rand()%4];
@@ -18,5 +19,7 @@ class Bunny{
 
 int main(){
    Bunny bun;
+   std::cout<<srand (time (NULL));//WORK ON THIS
+   //std::cout<<"Sex "<<bun.getSex()<<"\nRadioactive "<<bun.getRad()<<"\nAge "<<bun.getAge()<<"\nColor "<<bun.getColor()<<"\nName "<<bun.getName();
    return 0;
 }
