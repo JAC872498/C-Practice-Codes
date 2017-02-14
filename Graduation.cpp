0322f3b478ee219e96ffdf524deba2c62c888939
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-02-14 14:30:49 -0600

    Update Graduation.cpp

--- Graduation.cpp
+++ Graduation.cpp
@@ -3,13 +3,15 @@
 class Bunny{
     bool sex=rand()%2==0;//0\true==female; 1\false==male
     bool rad=rand()%50==0;//0\true==rad; 1-49\false=not rad
-    int age=1;
-    int color=3;
-    int name=5;
+    int age=123;
+    std::string color="Blue";
+    std::string name="Bob";
     public:
-    int getColor(){
-        return color;
-    }
+    bool getSex(){return sex;}
+    bool getRad(){return rad;}
+    int getAge(){return age;}
+    std::string getColor(){return color;}
+    std::string getName(){return name;}
 };
 
 int main(){
