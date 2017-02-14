1c9aaf2e23593d9c502357fd7e8fbf796aec2771
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-02-14 12:43:26 -0600

    Update Graduation.cpp

--- Graduation.cpp
+++ Graduation.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
 
+class Bunny{
+    bool sex=true;
+    bool rad=true;
+    int age=1;
+    int color=3;
+    int name=5;
+};
+
+int main(){
+   std::cout<<"Hello, world!";
+   return 0;
+}
