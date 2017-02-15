5525e3e050e6e29c6b4082addf08cfe95e6b0703
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-02-15 12:25:32 -0600

    Update Graduation.cpp

--- Graduation.cpp
+++ Graduation.cpp
@@ -14,15 +14,14 @@ class Bunny{
         std::string name=names[rand()%10];
         void setAll(int num1, int num2, int num3, int num4){sex=num1;rad=(num2==0);color=colors[num3];name=names[num4];}
         void description(){std::cout<<"Sex "<<sex<<"\nRadioactive "<<rad<<"\nAge "<<age<<"\nColor "<<color<<"\nName "<<name<<"\n";}
+
 };
 
 int main(){
-   Bunny bun1,bun2,bun3,bun4,bun5;
+   Bunny test[5];
    srand(time(0));
-   bun1.setAll(rand()%2,rand()%50,rand()%4,rand()%10);
-   bun2.setAll(rand()%2,rand()%50,rand()%4,rand()%10);
-   bun3.setAll(rand()%2,rand()%50,rand()%4,rand()%10);
-   bun4.setAll(rand()%2,rand()%50,rand()%4,rand()%10);
-   bun5.setAll(rand()%2,rand()%50,rand()%4,rand()%10);
+   for(int x=0;x<5;x++){
+       test[x].setAll(rand()%2,rand()%50,rand()%4,rand()%10);
+   }
    return 0;
 }
