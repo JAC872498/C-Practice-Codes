1dbf750b0c2b08bbdb5a05f1accdea66ea6983bc
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-02-15 10:01:58 -0600

    Update Graduation.cpp

--- Graduation.cpp
+++ Graduation.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 
 class Bunny{
-    bool sex=rand()%2==0;//0\true==female; 1\false==male
+    bool sex=rand()%2;//1\true==female; 0\false==male
     bool rad=rand()%50==0;//0\true==rad; 1-49\false=not rad
     int age=1;
     std::string colors[4]={"White","Brown","Black","Spotted"};
@@ -11,8 +11,8 @@ class Bunny{
     std::string color=colors[rand()%4];
     std::string name=names[rand()%10];
     public:
-        void setAll(int num1, int num2, int num3, int num4){sex=(num1==0);rad=(num2==0);color=colors[num3];name=names[num4];}
-        void getAll(){std::cout<<"Sex "<<sex<<"\nRadioactive "<<rad<<"\nAge "<<age<<"\nColor "<<color<<"\nName "<<name<<"\n";}
+        void setAll(int num1, int num2, int num3, int num4){sex=num1;rad=(num2==0);color=colors[num3];name=names[num4];}
+        void description(){std::cout<<"Sex "<<sex<<"\nRadioactive "<<rad<<"\nAge "<<age<<"\nColor "<<color<<"\nName "<<name<<"\n";}
         bool getSex(){return sex;}
         bool getRad(){return rad;}
         int getAge(){return age;}
@@ -25,7 +25,7 @@ class Bunny{
 int main(){
    Bunny bun;
    srand(time(0));
-   bun.setAll(rand()%2,rand()%50,rand()%4,rand()%10);
-   bun.getAll();
+   bun.setAll(rand()%100,rand()%50,rand()%4,rand()%10);
+   bun.description();
    return 0;
 }
