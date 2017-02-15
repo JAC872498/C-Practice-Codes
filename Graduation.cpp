41a30153ee5813bd771f1f4db6632a988cda91f8
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-02-15 08:24:16 -0600

    Update Graduation.cpp

--- Graduation.cpp
+++ Graduation.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 class Bunny{
     bool sex=rand()%2==0;//0\true==female; 1\false==male
     bool rad=rand()%50==0;//0\true==rad; 1-49\false=not rad
     int age=1;
-    int seed = static_cast<int>(time(0));
     std::string colors[4]={"White","Brown","Black","Spotted"};
     std::string names[10]={"Bob","Crigg","Doorknob","Oil","Slam","Skellet0n","Jew","Emo Chick","T3mmi","3l1t3 hax0r"};
     std::string color=colors[rand()%4];
@@ -19,7 +20,8 @@ class Bunny{
 
 int main(){
    Bunny bun;
-   std::cout<<srand (time (NULL));//WORK ON THIS
-   //std::cout<<"Sex "<<bun.getSex()<<"\nRadioactive "<<bun.getRad()<<"\nAge "<<bun.getAge()<<"\nColor "<<bun.getColor()<<"\nName "<<bun.getName();
+   int seed = static_cast<int>(time(0));
+   srand(time(NULL));
+   std::cout<<"Sex "<<bun.getSex()<<"\nRadioactive "<<bun.getRad()<<"\nAge "<<bun.getAge()<<"\nColor "<<bun.getColor()<<"\nName "<<bun.getName();
    return 0;
 }
