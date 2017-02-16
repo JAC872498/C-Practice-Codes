9a3db2f3b144b46ffaa6c834eabee73c15b8a860
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-02-16 11:19:18 -0600

    Update Graduation.cpp

--- Graduation.cpp
+++ Graduation.cpp
@@ -8,9 +8,10 @@ class Bunny{
         std::string colors[4]={"White","Brown","Black","Spotted"};
         std::string names[10]={"Bob","Crigg","Doorknob","Oil","Slam","Skellet0n","Jew","Emo Chick","T3mmi","3l1t3 hax0r"};
     public:
-        int age=1;
+        int age=0;
         bool sex=rand()%2;//1\true==female; 0\false==male
         bool rad=rand()%50==0;//0\true==rad; 1-49\false=not rad
+        bool alive=1;
         std::string color=colors[rand()%4];
         std::string name=names[rand()%10];
         void setAll(int num1, int num2, int num3, int num4){sex=num1;rad=(num2==0);color=colors[num3];name=names[num4];}
@@ -18,16 +19,34 @@ class Bunny{
 
 };
 
+int mul(std::vector<Bunny> stuff){//0==all bunnies dead, 1==no new bunnies, 2==new bunnies
+    bool male=0;
+    bool female=0;
+    for(int x=0;x<stuff.size();x++){
+        if(stuff[x].sex==1 && stuff[x].alive==1){
+            female=1;
+        }else if(stuff[x].sex==0 && stuff[x].alive==1){
+            male=1;
+        }
+    }
+    if(male && female){
+        return 2;
+    }else if(male || female){
+        return 1;
+    }return 0;
+}
+
 int main(){
     srand(time(0));
     std::vector<Bunny>bunny(5);
-    for(int x=0;x<vecsize;x++){
+    for(int x=0;x<bunny.size();x++){
         bunny[x].setAll(rand()%2,rand()%50,rand()%4,rand()%10);
         /*bunny[x].description();
         std::cout<<"\n";*/
     }
     for(int x=0;x<3;x++){
-        for(int x=0;x<vecsize;x++){bunny[x].age=bunny[x].age+1;}
+        for(int x=0;x<bunny.size();x++){bunny[x].age=bunny[x].age+1;}
+        //std::cout<<bunny[0].age;
     }
     return 0;
 }
