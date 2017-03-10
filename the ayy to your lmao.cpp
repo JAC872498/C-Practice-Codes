643ab83bf018c5cec110833125c0f496866a904e
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-10 10:49:49 -0600

    Create the ayy to your lmao.cpp

---
+++ the ayy to your lmao.cpp
@@ -0,0 +1,13 @@
+//Tic-tak-toe because I'm incredibly bored and have nothing else to do
+#include <iostream>
+#include <string>
+
+int main(){
+	srand (time(NULL));
+	std::string grid[3][3]={
+  	{"-","-","-"},
+    {"-","-","-"},
+    {"-","-","-"}
+  };
+  return 0;
+}
