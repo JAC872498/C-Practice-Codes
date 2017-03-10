961dfc21f3755e89eced10a44ad67313d51db696
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-10 10:58:33 -0600

    Update the ayy to your lmao.cpp

--- the ayy to your lmao.cpp
+++ the ayy to your lmao.cpp
@@ -1,13 +1,19 @@
 //Tic-tak-toe because I'm incredibly bored and have nothing else to do
 #include <iostream>
 #include <string>
+#include <time.h>
 
 int main(){
-	srand (time(NULL));
-	std::string grid[3][3]={
-  	{"-","-","-"},
+    srand (time(NULL));
+  std::string grid[3][3]={
+    {"-","-","-"},
     {"-","-","-"},
     {"-","-","-"}
   };
+  if(rand()%2==0){//You start first
+      
+  }else{//Com start first
+      
+  }
   return 0;
 }
