e6f97510cda7e9eba4679597fe360aa3b3bae6d9
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-12 13:59:41 -0500

    Update The Ayy To Your Lmao.cpp

--- The Ayy To Your Lmao.cpp
+++ The Ayy To Your Lmao.cpp
@@ -4,9 +4,8 @@
 #include <string>
 #include <time.h>
 #include <stdlib.h>
-int win(std::string grid[][3], std::string myChoice, std::string comChoice, int turns){//0==no win, 1==you win, 2==computer win
-    if(turns<5){return 0;}
-	if(grid[0][0].compare(myChoice)&&grid[1][0].compare(myChoice)&&grid[2][0].compare(myChoice)){return 1;}//You winning
+int win(std::string grid[][3], std::string myChoice, std::string comChoice){//0==no win, 1==you win, 2==computer win
+    if(grid[0][0].compare(myChoice)&&grid[1][0].compare(myChoice)&&grid[2][0].compare(myChoice)){return 1;}//You winning
     else if(grid[0][1].compare(myChoice)&&grid[1][1].compare(myChoice)&&grid[2][1].compare(myChoice)){return 1;}
     else if(grid[0][2].compare(myChoice)&&grid[1][2].compare(myChoice)&&grid[1][2].compare(myChoice)){return 1;}
     else if(grid[0][0].compare(myChoice)&&grid[0][1].compare(myChoice)&&grid[0][2].compare(myChoice)){return 1;}
@@ -43,7 +42,10 @@ int main(){
   if(rand()%2==0){//You start first
       myChoice="X";
       comChoice="O";
-      std::cout<<"You start\n";
+      std::cout<<"You start\nEnter X\n";
+	  std::cin>>myX;
+	  std::cout<<"Enter Y\n";
+	  std::cin>>myY;
       grid[myX-1][myY-1]=myChoice;
   }else{//Com start first
       myChoice="O";
@@ -52,16 +54,15 @@ int main(){
       grid[comX][comY]=comChoice;
   }
   turns++;
-   while(win(grid, myChoice, comChoice, turns)==0){
+   while(win(grid, myChoice, comChoice)==0){
       for(int x=0;x<3;x++){for(int y=0;y<3;y++){std::cout<<grid[x][y];}std::cout<<"\n";}
 	  if((turns%2==0&&myChoice.compare("X"))||(turns%2!=0&&myChoice.compare("O"))){
 		  std::cout<<"Enemy goes\n";
 		  comX=rand()%3;
 		  comY=rand()%3;
-		  grid[comX][comY]=comChoice;
+		  grid[comY][comX]=comChoice;
 	  }else{
-		  std::cout<<"You go\n";
-		  std::cout<<"Enter X\n";
+		  std::cout<<"You go\nEnter X\n";
 		  std::cin>>myX;
 		  std::cout<<"Enter Y\n";
 		  std::cin>>myY;
@@ -69,7 +70,7 @@ int main(){
 	  }
 	  turns++;
   }
-  if(win(grid, myChoice, comChoice, turns)==1){
+  if(win(grid, myChoice, comChoice)==1){
 	  std::cout<<"You win";
   }else{
 	  std::cout<<"Enemy wins";
