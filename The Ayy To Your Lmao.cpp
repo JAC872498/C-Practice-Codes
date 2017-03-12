f40a22d23ad01bf2a759a614a8080ad4fa9ee209
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-03-12 15:18:47 -0500

    Update The Ayy To Your Lmao.cpp

--- The Ayy To Your Lmao.cpp
+++ The Ayy To Your Lmao.cpp
@@ -5,43 +5,42 @@
 #include <time.h>
 #include <stdlib.h>
 int win(std::string grid[][3], std::string myChoice, std::string comChoice){//0==no win, 1==you win, 2==computer win
-    /*if(grid[0][0].compare(myChoice)&&grid[1][0].compare(myChoice)&&grid[2][0].compare(myChoice)){return 1;}//You winning
-    else if(grid[0][1].compare(myChoice)&&grid[1][1].compare(myChoice)&&grid[2][1].compare(myChoice)){return 1;}
-    else if(grid[0][2].compare(myChoice)&&grid[1][2].compare(myChoice)&&grid[1][2].compare(myChoice)){return 1;}
-    else if(grid[0][0].compare(myChoice)&&grid[0][1].compare(myChoice)&&grid[0][2].compare(myChoice)){return 1;}
-    else if(grid[1][0].compare(myChoice)&&grid[1][1].compare(myChoice)&&grid[1][2].compare(myChoice)){return 1;}
-    else if(grid[2][0].compare(myChoice)&&grid[2][1].compare(myChoice)&&grid[2][2].compare(myChoice)){return 1;}
-    else if(grid[0][0].compare(myChoice)&&grid[1][1].compare(myChoice)&&grid[2][2].compare(myChoice)){return 1;}
-    else if(grid[0][2].compare(myChoice)&&grid[1][1].compare(myChoice)&&grid[2][0].compare(myChoice)){return 1;}
+    if(grid[0][0]==(myChoice)&&grid[1][0]==(myChoice)&&grid[2][0]==(myChoice)){return 1;}//You winning
+    else if(grid[0][1]==(myChoice)&&grid[1][1]==(myChoice)&&grid[2][1]==(myChoice)){return 1;}
+    else if(grid[0][2]==(myChoice)&&grid[1][2]==(myChoice)&&grid[1][2]==(myChoice)){return 1;}
+    else if(grid[0][0]==(myChoice)&&grid[0][1]==(myChoice)&&grid[0][2]==(myChoice)){return 1;}
+    else if(grid[1][0]==(myChoice)&&grid[1][1]==(myChoice)&&grid[1][2]==(myChoice)){return 1;}
+    else if(grid[2][0]==(myChoice)&&grid[2][1]==(myChoice)&&grid[2][2]==(myChoice)){return 1;}
+    else if(grid[0][0]==(myChoice)&&grid[1][1]==(myChoice)&&grid[2][2]==(myChoice)){return 1;}
+    else if(grid[0][2]==(myChoice)&&grid[1][1]==(myChoice)&&grid[2][0]==(myChoice)){return 1;}
     
-    if(grid[0][0].compare(comChoice)&&grid[1][0].compare(comChoice)&&grid[2][0].compare(comChoice)){return 1;}//You winning
-    else if(grid[0][1].compare(comChoice)&&grid[1][1].compare(comChoice)&&grid[2][1].compare(comChoice)){return 1;}
-    else if(grid[0][2].compare(comChoice)&&grid[1][2].compare(comChoice)&&grid[1][2].compare(comChoice)){return 1;}
-    else if(grid[0][0].compare(comChoice)&&grid[0][1].compare(comChoice)&&grid[0][2].compare(comChoice)){return 1;}
-    else if(grid[1][0].compare(comChoice)&&grid[1][1].compare(comChoice)&&grid[1][2].compare(comChoice)){return 1;}
-    else if(grid[2][0].compare(comChoice)&&grid[2][1].compare(comChoice)&&grid[2][2].compare(comChoice)){return 1;}
-    else if(grid[0][0].compare(comChoice)&&grid[1][1].compare(comChoice)&&grid[2][2].compare(comChoice)){return 1;}
-    else if(grid[0][2].compare(comChoice)&&grid[1][1].compare(comChoice)&&grid[2][0].compare(comChoice)){return 1;}
-    */return 0;
+    if(grid[0][0]==(comChoice)&&grid[1][0]==(comChoice)&&grid[2][0]==(comChoice)){return 2;}//You winning
+    else if(grid[0][1]==(comChoice)&&grid[1][1]==(comChoice)&&grid[2][1]==(comChoice)){return 2;}
+    else if(grid[0][2]==(comChoice)&&grid[1][2]==(comChoice)&&grid[1][2]==(comChoice)){return 2;}
+    else if(grid[0][0]==(comChoice)&&grid[0][1]==(comChoice)&&grid[0][2]==(comChoice)){return 2;}
+    else if(grid[1][0]==(comChoice)&&grid[1][1]==(comChoice)&&grid[1][2]==(comChoice)){return 2;}
+    else if(grid[2][0]==(comChoice)&&grid[2][1]==(comChoice)&&grid[2][2]==(comChoice)){return 2;}
+    else if(grid[0][0]==(comChoice)&&grid[1][1]==(comChoice)&&grid[2][2]==(comChoice)){return 2;}
+    else if(grid[0][2]==(comChoice)&&grid[1][1]==(comChoice)&&grid[2][0]==(comChoice)){return 2;}
+    return 0;
 }
 
 int main(){
   srand(time(NULL));
   int myX=5;
   int myY=2;
-  std::string myChoice;
+  std::string myChoice="X";
   int comX=rand()%3;
   int comY=rand()%3;
   int turns=0; 
-  std::string comChoice;
+  std::string comChoice="O";
   std::string grid[3][3]={
     {"-","-","-"},
     {"-","-","-"},
     {"-","-","-"}
   };
+  std::cout<<win(grid, myChoice, comChoice);
   if(rand()%2==0){//You start first
-      myChoice="X";
-      comChoice="O";
       std::cout<<"You start\nEnter X\n";
 	  std::cin>>myX;
 	  std::cout<<"Enter Y\n";
@@ -59,7 +58,7 @@ int main(){
 		  break;
 	  }
       for(int x=0;x<3;x++){for(int y=0;y<3;y++){std::cout<<grid[y][x];}std::cout<<"\n";}
-	  if((turns%2==0&&myChoice.compare("X"))||(turns%2!=0&&myChoice.compare("O"))){
+	  if((turns%2==0&&myChoice==("X"))||(turns%2!=0&&myChoice==("O"))){
 		  std::cout<<"Enemy goes\n";
 		  comX=rand()%3;
  	  	  comY=rand()%3;
