4d9d7a21e8adc045265fd1ad3f27206692e2953e
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-04-19 12:14:48 -0500

    Update Game O' Chess.cpp

--- Game O' Chess.cpp
+++ Game O' Chess.cpp
@@ -8,8 +8,8 @@ class Piece{//0=king, 1=queen, 2=rook, 3=knight, 4=bishop, 5=pawn
         bool isWhite, isTaken;
         std::string type, types[12]={"K","Q","R","N","B","P","k","q","r","n","b","p"};
         void setAll(int setTypeNum, int setXPos, int setYPos, bool setIsWhite, bool setIsTaken){
-            xPos=setXPos;
-            yPos=setYPos;
+            xPos=setXPos-1;
+            yPos=setYPos-1;
             isWhite=setIsWhite;
             isTaken=setIsTaken;
             if(isWhite){
@@ -21,7 +21,7 @@ class Piece{//0=king, 1=queen, 2=rook, 3=knight, 4=bishop, 5=pawn
 };
 
 int main(){
-    std::string grid[8][8]={
+    std::string board[8][8]={
         {".",".",".",".",".",".",".","."},
         {".",".",".",".",".",".",".","."},
         {".",".",".",".",".",".",".","."},
@@ -31,6 +31,9 @@ int main(){
         {".",".",".",".",".",".",".","."},
         {".",".",".",".",".",".",".","."}
     };
-    for(int x=0;x<8;x++){for(int y=0;y<8;y++){std::cout<<grid[x][y];}std::cout<<"\n";}
+    /*Piece testKing;
+    testKing.setAll(0,1,1,true,false);
+    board[testKing.xPos][testKing.yPos]=testKing.type;*/
+    for(int x=0;x<8;x++){for(int y=0;y<8;y++){std::cout<<board[x][y];}std::cout<<"\n";}
     return 0;
 }
