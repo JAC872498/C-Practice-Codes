56ce12a447c2b74c20416a1b304697e36528772f
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-04-19 14:44:20 -0500

    Update Game O' Chess.cpp

--- Game O' Chess.cpp
+++ Game O' Chess.cpp
@@ -23,14 +23,28 @@ class Piece{//0=king, 1=queen, 2=rook, 3=knight, 4=bishop, 5=pawn
         bool canMoveTo(int moveToX, int moveToY){
             if((xPos==moveToX&&yPos==moveToY)||moveToX<0||moveToY<0||moveToX>=8||moveToY>=8){
                 return false;
+            }else if(type=="K"||type=="k"){
+                
+            }else if(type=="Q"||type=="q"){
+                
+            }else if(type=="R"||type=="r"){
+                
+            }else if(type=="N"||type=="n"){
+                
+            }else if(type=="B"||type=="b"){
+                
+            }else if(type=="P"){//moves up
+                
+            }else if(type=="p"){//moves down
+                
             }return true;
         }
 };
 
 int main(){
     std::string board[8][8]={
-        {".",".",".",".",".",".",".","."},
-        {".",".",".",".",".",".",".","."},
+        {"1","3",".",".",".",".",".","."},
+        {"2",".",".",".",".",".",".","."},
         {".",".",".",".",".",".",".","."},
         {".",".",".",".",".",".",".","."},
         {".",".",".",".",".",".",".","."},
@@ -41,7 +55,7 @@ int main(){
     Piece testKing;
     testKing.setAll(0,1,1,true,false);
     board[testKing.xPos][testKing.yPos]=testKing.type;
-    for(int x=0;x<8;x++){for(int y=0;y<8;y++){std::cout<<board[x][y];}std::cout<<"\n";}
-    std::cout<<testKing.canMoveTo(10,-4);
+    for(int y=0;y<8;y++){for(int x=0;x<8;x++){std::cout<<board[y][x];}std::cout<<"\n";}
+    std::cout<<testKing.canMoveTo(3,4);
     return 0;
 }
