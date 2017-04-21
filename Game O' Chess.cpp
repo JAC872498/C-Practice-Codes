286c0d2d2d5e2814569b650b0ee2c3db3c448d17
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-04-21 14:32:42 -0500

    Update Game O' Chess.cpp

--- Game O' Chess.cpp
+++ Game O' Chess.cpp
@@ -6,7 +6,7 @@ class Piece{
     public:
         int row, column;
         bool isWhite, isTaken;
-        std::string type, types[15]={"R","N","B","Q","K","B","N","R","P","k","q","b","n","r","p"};
+        std::string type, types[15]={"R","N","B","Q","K","B","N","R","P","r","n","b","q","k","b","n","r","p"};
         
         void setAll(int setTypeNum, int setRow, int setColumn, bool setIsWhite, bool setIsTaken){
             row=setRow-1;
@@ -55,7 +55,7 @@ int main(){
     /*White pieces*/Piece whiteKing, whiteQueen, whiteRook1, whiteRook2, whiteKnight1, whiteKnight2, whiteBishop1, whiteBishop2, whitePawn1, whitePawn2, whitePawn3, whitePawn4, whitePawn5, whitePawn6, whitePawn7, whitePawn8;
     /*Black pieces*/Piece blakcKing, blackQueen, blackRook1, blackRook2, blackKnight1, blackKnight2, blackBishop1, blackBishop2, blackPawn1, blackPawn2, blackPawn3, blackPawn4, blackPawn5, blackPawn6, balckPawn7, blackPawn8;
     Piece whiteFleet[16]={whiteRook1, whiteKnight1, whiteBishop1, whiteQueen, whiteKing, whiteBishop2, whiteKnight2, whiteRook2, whitePawn1, whitePawn2, whitePawn3, whitePawn4, whitePawn5, whitePawn6, whitePawn7, whitePawn8};
-    Piece blackFleet[16]={blakcKing, blackQueen, blackRook1, blackRook2, blackKnight1, blackKnight2, blackBishop1, blackBishop2, blackPawn1, blackPawn2, blackPawn3, blackPawn4, blackPawn5, blackPawn6, balckPawn7, blackPawn8};
+    Piece blackFleet[16]={blackRook1, blackKnight1, blackBishop1, blackQueen, blackKing, blackBishop2, blackKnight2, blackRook2, blackPawn1, blackPawn2, blackPawn3, blackPawn4, blackPawn5, blackPawn6, blackPawn7, blackPawn8};
     for(int pawnNum=8;pawnNum<16;pawnNum++){
         whiteFleet[pawnNum].setAll(5, 7, pawnNum-7, true, false);
         blackFleet[pawnNum].setAll(5, 2, pawnNum-7, false, false);
@@ -64,7 +64,9 @@ int main(){
     }
     for(int pieceNum=0;pieceNum<8;pieceNum++){
         whiteFleet[pieceNum].setAll(pieceNum, 8, pieceNum+1, true, false);
+        blackFleet[pieceNum+8].setAll(pieceNum, 1, pieceNum+1, false, false);
         board[whiteFleet[pieceNum].row][whiteFleet[pieceNum].column]=whiteFleet[pieceNum].type;
+        board[blackFleet[pieceNum].row][blackFleet[pieceNum].column]=blackFleet[pieceNum+8].type;
     }
     for(int row=0;row<8;row++){for(int column=0;column<8;column++){std::cout<<board[row][column];}std::cout<<"\n";}
     return 0;
