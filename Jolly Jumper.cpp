47891b8aac10af19de37569dd4f48e1b2b4135c8
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-05-08 13:50:47 -0500

    Update Jolly Jumper.cpp

--- Jolly Jumper.cpp
+++ Jolly Jumper.cpp
@@ -3,12 +3,13 @@
 #include <iostream>
 
 int main(){
-    int total;
+    int total,length;
     while(true){
         std::cin>>total;
         if(!std::cin){break;}
         std::cout<<total<<"\n";
+        length++;
     }
-    std::cout<<"XD";
+    std::cout<<length;
     return 0;
 }
