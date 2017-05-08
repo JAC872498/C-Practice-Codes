a1743e06d07961cb8f6a2179eadc2661c2e02ab6
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-05-08 11:57:55 -0500

    Update Jolly Jumper.cpp

--- Jolly Jumper.cpp
+++ Jolly Jumper.cpp
@@ -1,1 +1,16 @@
 /**/
+
+#include <iostream>
+
+int main(){
+    int currInt, nextInt;
+    while(true){
+        std::cin>>currInt>>nextInt;
+        if(currInt==||nextInt==){
+            break;
+        }
+    }
+    if(isJolly) std::cout<<"0";
+    else std::cout<<"1";
+    return 0;
+}
