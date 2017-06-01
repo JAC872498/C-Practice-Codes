b0ef3be245cdd4faae751fe12d10fecb56785093
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-01 17:20:17 -0500

    Update Hartals.cpp

--- Hartals.cpp
+++ Hartals.cpp
@@ -3,28 +3,28 @@
 #include <iostream>
 
 int main(){
-	int days, parties, lost=0;
+	int times, days, parties;
 	bool counts=true;//dosn't count if on fri or sat, day 1 is sun
-	//std::cin>>times;
-	//for(int i=0;i<times;i++){
-	std::cin>>days>>parties;
-	int strikes[parties];
-	for(int x=0;x<parties;x++){
-	    std::cin>>strikes[x];
+	std::cin>>times;
+	for(int i=0;i<times;i++){
+		std::cin>>days>>parties;
+		int strikes[parties], lost=0;
+		for(int x=0;x<parties;x++){
+	    	std::cin>>strikes[x];
+		}
+		for(int day=1;day<=days;day++){
+			counts=true;
+	    	if(day%7==0||(day+1)%7==0) counts=false;
+   			if(counts){
+    			for(int x=0;x<parties;x++){
+	           		if(day%strikes[x]==0){
+	            	   	lost+=1;
+	                	break;
+					}
+				}
+			}
+		}
+		std::cout<<lost<<" days would be lost."<<"\n";
 	}
-	for(int day=1;day<=days;day++){
-	    counts=true;
-	    if(day%7==0||(day+1)%7==0) counts=false;
-	    if(counts){
-	        for(int x=0;x<parties;x++){
-	            if(day%strikes[x]==0){
-	                lost+=1;
-	                break;
-	            }
-	        }
-	    }
-	}
-	std::cout<<lost;
-	//}
 	return 0;
 }
