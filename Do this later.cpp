72d4dce1f10b95429d1476f54177d00dee7aa98e
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-12 20:30:04 -0500

    Create Do this later.cpp

--- Do this later.cpp
+++ Do this later.cpp
@@ -1,1 +1,14 @@
 /**/
+
+#include <iostream>
+#include <string>
+
+int main(){
+	int cases,n;
+	std::cin>>cases>>n;
+	std::string cans[n],name;
+	for(int x=0;x<n;x++){
+		std::cin>>cans[x];
+	}
+	return 0;
+}
