a6dcb94970580ca2061a3cf0b79ac587442396ab
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-12 21:52:49 -0500

    Update Do this later.cpp

--- Do this later.cpp
+++ Do this later.cpp
@@ -4,35 +4,33 @@
 #include <string>
 
 int main(){
-	int cases,n,i=0,vote,top=0,topX;
+	int cases,n,c=0,vote,curr=1,ballots=0;
 	std::cin>>cases>>n;
 	std::string names[n];
-	int votes[n];
+	int votes[1][n];
+	int voted[1][n];
 	for(int x=0;x<n;x++){
 		std::cin>>names[x];
 	}
 	while(vote!=0){
-		/*std::cin>>vote;
-		i++;
-		if(i==n){
-			i=0;
-		}
-		votes[i]=n-vote;*/
-		for(int i=0;i<=n;i++){
+		for(int i=0;i<n;i++){
 			std::cin>>vote;
-			votes[i]=n-vote+1;
 			if(vote==0){
-				break;
+				goto calc;
 			}
+			votes[c][i]=vote;
 		}
+		c++;//ayy
 	}
+	calc:
 	for(int x=0;x<n;x++){
-		std::cout<<votes[x]<<"\n";
-		if(votes[x]>top){
-			top=votes[x];
-			topX=x;
+		std::cout<<names[x]<<" ";
+	}std::cout<<"\n";
+	for(int y=0;y<c;y++){
+		for(int x=0;x<n;x++){
+			std::cout<<votes[y][x]<<" ";
 		}
+		std::cout<<"\n";
 	}
-	std::cout<<names[topX];
 	return 0;
 }
