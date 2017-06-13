27e38ea292ae719df3b672deec7098f56fbcee56
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-12 21:04:16 -0500

    Update Do this later.cpp

--- Do this later.cpp
+++ Do this later.cpp
@@ -4,7 +4,7 @@
 #include <string>
 
 int main(){
-	int cases,n,i,vote=1;
+	int cases,n,i=0,vote=1;
 	std::cin>>cases>>n;
 	std::string names[n];
 	int votes[n];
@@ -13,12 +13,19 @@ int main(){
 		//getline(std::cin, names[x]);
 	}
 	while(vote!=0){
-		std::cin>>vote;
+		/*std::cin>>vote;
 		i++;
 		if(i==n){
 			i=0;
 		}
-		votes[i]=n-i;
+		votes[i]=n-vote;*/
+		for(int i=0;i<n;i++){
+			std::cin>>vote;
+			votes[i]=n-votel
+		}
+	}
+	for(int x=0;x<n;x++){
+		std::cout<<votes[x]<<"\n";
 	}
 	return 0;
 }
