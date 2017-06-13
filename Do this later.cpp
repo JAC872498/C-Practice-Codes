1c33b700ad823b05f36635938a8a3748c07cd1e3
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-06-12 22:18:10 -0500

    Update Do this later.cpp

--- Do this later.cpp
+++ Do this later.cpp
@@ -7,10 +7,11 @@ int main(){
 	int cases,n,c=0,vote,curr=1,ballots=0;
 	std::cin>>cases>>n;
 	std::string names[n];
-	int votes[1][n];
-	int voted[1][n];
+	int votes[1000][n];
+	int voted[n];
 	for(int x=0;x<n;x++){
 		std::cin>>names[x];
+		voted[x]=0;
 	}
 	while(vote!=0){
 		for(int i=0;i<n;i++){
@@ -29,8 +30,14 @@ int main(){
 	for(int y=0;y<c;y++){
 		for(int x=0;x<n;x++){
 			std::cout<<votes[y][x]<<" ";
+			if(votes[y][x]==curr){
+				voted[x]++;
+			}
 		}
 		std::cout<<"\n";
+	}std::cout<<"\n";
+	for(int x=0;x<n;x++){
+		std::cout<<voted[x];
 	}
 	return 0;
 }
