65c83ffaf35b273f00fb694950d69f2899eb1442
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-08-07 21:47:45 -0500

    Update ShellSort.cpp

--- ShellSort.cpp
+++ ShellSort.cpp
@@ -1,1 +1,26 @@
 /**/
+
+#include <iostream>
+
+int main(){
+	int amount,curr=0;
+	std::cin>>amount;
+	std::string names[amount];
+	std::string name;
+	int scores[amount];
+	for(int x=0;x<6;x++){
+		std::cin>>name;
+		for(int n=0;n<curr;n++){
+			if(name==names[n]){
+				scores[n]+=1;
+				goto found;
+			}
+		}
+		names[curr]=name;
+		scores[curr]=0;
+		curr+=1;
+		found:
+		std::cout<<"";
+	}
+	return 0;
+}
