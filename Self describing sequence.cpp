ed0c0f7ac81157ae364c656a30138d68cd3f26f9
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-08-14 22:34:24 -0500

    Update Self describing sequence.cpp

--- Self describing sequence.cpp
+++ Self describing sequence.cpp
@@ -3,15 +3,15 @@
 #include <iostream>
 
 int main(){
-	int n=2,c=2;
+	int n=2,c=0;
 	int f[n];
 	f[0]=1;
 	f[1]=2;
 	for(int x=0;x<n;x++){
-		std::cout<<x+1<<" "<<f[x]<<"\n";
+		std::cout<<"n:"<<x+1<<" "<<"f(n):"<<f[x]<<"\n";
 		for(int y=0;y<f[x];y++){
-			std::cout<<x+1;
 			f[c]=x+1;
+			c+=1;
 		}
 	}
 	return 0;
