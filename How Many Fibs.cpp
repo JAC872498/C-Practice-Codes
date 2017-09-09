6dbffa8205681d1c3905c354a60dc80d895d6702
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-08 21:18:24 -0500

    Update How Many Fibs.cpp

--- How Many Fibs.cpp
+++ How Many Fibs.cpp
@@ -4,15 +4,21 @@
 #include <vector>
 
 int main(){
-	int startRagne=0,endRange=0;
+	int startRagne=0,endRange=0,tot=0;
 	std::cout<<"Enter your starting and ending numbers.\n";
 	std::cin>>startRagne>>endRange;
 	std::vector <int> fibs;
 	fibs.push_back(0);
 	fibs.push_back(1);
 	while(fibs[fibs.size()-1]<=endRange){
-		std::cout<<(fibs[fibs.size()-1])<<"\n";
 		fibs.push_back(fibs[fibs.size()-1]+fibs[fibs.size()-2]);
 	}
+	for(int n=0;n<fibs.size();n++){
+		if(fibs[n]>=startRagne&&fibs[n]<=endRange){
+			std::cout<<fibs[n]<<"\n";
+			tot+=1;
+		}
+	}
+	std::cout<<"There are "<<tot<<" total fibonnaci numbers inbetween those numbers.";
 	return 0;
 }
