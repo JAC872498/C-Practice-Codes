119434f9452929cdff860680012040d5ff6cdd93
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-09-08 21:13:21 -0500

    Update How Many Fibs.cpp

--- How Many Fibs.cpp
+++ How Many Fibs.cpp
@@ -11,7 +11,7 @@ int main(){
 	fibs.push_back(0);
 	fibs.push_back(1);
 	while(fibs[fibs.size()-1]<=endRange){
-		system.out.print(fibs[fibs.size()-1]);
+		std::cout<<(fibs[fibs.size()-1])<<"\n";
 		fibs.push_back(fibs[fibs.size()-1]+fibs[fibs.size()-2]);
 	}
 	return 0;
