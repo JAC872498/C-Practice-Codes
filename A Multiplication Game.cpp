646e999abab2fda483bf4b3d883892a428456fc1
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-10-26 12:36:23 -0500

    Update A Multiplication Game.cpp

--- A Multiplication Game.cpp
+++ A Multiplication Game.cpp
@@ -1,5 +1,14 @@
 /*pg 143 in http://acm.cs.buap.mx/downloads/Programming_Challenges.pdf*/
 
+#include <iostream>
+
 int main(){
-  return(0);
+    int n,p=1;
+    bool winner=false;
+    while(p<n){
+        
+    }
+    std::cin>>n;
+    std::cout<<n;
+    return(0);
 }
