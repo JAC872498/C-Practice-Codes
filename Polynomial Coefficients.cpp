597645589d79c5607f2329d8cedbbef90f3a4610
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-10-28 19:03:42 -0500

    Update Polynomial Coefficients.cpp

--- Polynomial Coefficients.cpp
+++ Polynomial Coefficients.cpp
@@ -1,6 +1,6 @@
 /*pg 144 in http://acm.cs.buap.mx/downloads/Programming_Challenges.pdf
 I have no idea if this is the correct way to do the problem
-The description was preaty terrible for this problem*/
+The description was terrible for this problem*/
 
 #include <iostream>
 #include <cmath>//pow(1,2)
