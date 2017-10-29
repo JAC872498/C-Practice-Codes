255a22e6774fc1e4f7db6ba546f5ce7835ecb1bc
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-10-28 19:02:49 -0500

    Update Polynomial Coefficients.cpp

--- Polynomial Coefficients.cpp
+++ Polynomial Coefficients.cpp
@@ -3,15 +3,16 @@ I have no idea if this is the correct way to do the problem
 The description was preaty terrible for this problem*/
 
 #include <iostream>
+#include <cmath>//pow(1,2)
 
 int main(){
-	int n,k,cur,tot=0;
+	int n,k,tot=0;
 	std::cin>>n>>k;
-	//int sequence[k];
+	int sequence[k];
 	for(int x=0;x<k;x++){
-		std::cin>>cur;
-		tot+=cur;
-		//std::cin>>sequence[x];
+		std::cin>>sequence[x];
+		tot+=pow(sequence[x],n);
 	}
+	std::cout<<tot;
     return(0);
 }
