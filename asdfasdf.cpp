745294feb57cf16e2bc469aa4022a333cbbd2e4d
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-11-07 11:44:52 -0600

    Update asdfasdf.cpp

--- asdfasdf.cpp
+++ asdfasdf.cpp
@@ -1,5 +1,11 @@
 /*pg # if http://acm.cs.buap.mx/downloads/Programming_Challenges.pdf*/
 
+// Example program
+#include <iostream>
+
 int main(){
-  return(0);
+  int xPos=1,yPos=1,jump=1;
+  for(int n=0;n<5;n++){
+      std::cout<<xPos<<" "<<yPos<<"\n";
+  }
 }
