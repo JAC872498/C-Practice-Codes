d10578cb57f64665c571a17c17693f37c119a813
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-11-07 12:14:03 -0600

    Update asdfasdf.cpp

--- asdfasdf.cpp
+++ asdfasdf.cpp
@@ -4,8 +4,8 @@
 
 int main(){
   int xPos=1,yPos=1,jump=1;
+  std::cout<<xPos<<" "<<yPos<<"\n";
   for(int n=0;n<1;n++){
-      std::cout<<xPos<<" "<<yPos<<"\n";
       yPos+=1;
       std::cout<<xPos<<" "<<yPos<<"\n";
       for(int x=0;x<jump;x++){
@@ -14,6 +14,17 @@ int main(){
       }
       for(int y=0;y<jump;y++){
           yPos-=1;
+          std::cout<<xPos<<" "<<yPos<<"\n";
+      }
+      jump+=1;
+      xPos+=1;
+      std::cout<<xPos<<" "<<yPos<<"\n";
+      for(int y=0;y<jump;y++){
+          yPos+=1;
+          std::cout<<xPos<<" "<<yPos<<"\n";
+      }
+      for(int x=0;x<jump;x++){
+          xPos-=1;
           std::cout<<xPos<<" "<<yPos<<"\n";
       }
       jump+=1;
