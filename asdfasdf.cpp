b59e6ce2f45b7865e54316190664c699b5bb2cc8
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-11-07 12:06:48 -0600

    Update asdfasdf.cpp

--- asdfasdf.cpp
+++ asdfasdf.cpp
@@ -1,11 +1,21 @@
 /*pg # if http://acm.cs.buap.mx/downloads/Programming_Challenges.pdf*/
 
-// Example program
 #include <iostream>
 
 int main(){
   int xPos=1,yPos=1,jump=1;
-  for(int n=0;n<5;n++){
+  for(int n=0;n<1;n++){
       std::cout<<xPos<<" "<<yPos<<"\n";
+      yPos+=1;
+      std::cout<<xPos<<" "<<yPos<<"\n";
+      for(int x=0;x<jump;x++){
+          xPos+=1;   
+          std::cout<<xPos<<" "<<yPos<<"\n";
+      }
+      for(int y=0;y<jump;y++){
+          yPos+=1;
+          std::cout<<xPos<<" "<<yPos<<"\n";
+      }
+      jump+=1;
   }
 }
