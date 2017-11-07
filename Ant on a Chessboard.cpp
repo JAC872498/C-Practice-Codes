dfac2dcaf4331c71eaa1adc14c104690910ab035
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-11-07 12:55:51 -0600

    Update and rename asdfasdf.cpp to Ant on a Chessboard.cpp

--- Ant on a Chessboard.cpp
+++ Ant on a Chessboard.cpp
@@ -1,11 +1,12 @@
-/*pg 299 if http://acm.cs.buap.mx/downloads/Programming_Challenges.pdf*/
+/*pg # if http://acm.cs.buap.mx/downloads/Programming_Challenges.pdf*/
 
 #include <iostream>
 
 int main(){
-  int xPos=1,yPos=1,jump=1,move=1;
+  int xPos=1,yPos=1,jump=0,move=1;
   std::cout<<move<<" "<<xPos<<" "<<yPos<<"\n";
-  for(int n=0;n<1;n++){
+  for(int n=0;n<3;n++){
+      jump+=1;
       yPos+=1;
       move+=1;
       std::cout<<move<<" "<<xPos<<" "<<yPos<<"\n";
@@ -33,7 +34,5 @@ int main(){
           move+=1;
           std::cout<<move<<" "<<xPos<<" "<<yPos<<"\n";
       }
-      jump+=1;
-      move+=1;
   }
 }
