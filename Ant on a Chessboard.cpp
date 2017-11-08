064e7fc373aafdd5fe05ebfd227776a25f3f54a0
Jordan Camilletti <jordan.camilletti@gmail.com>
2017-11-08 12:01:24 -0600

    Update Ant on a Chessboard.cpp

--- Ant on a Chessboard.cpp
+++ Ant on a Chessboard.cpp
@@ -1,3 +1,5 @@
+//Page 299 in http://acm.cs.buap.mx/downloads/Programming_Challenges.pdf
+
 #include <iostream>
 #include <iomanip>
 
@@ -5,41 +7,34 @@ int main(){
   int xPos=0,yPos=0,jump=0,move=1;
   int graph[5][5];
   graph[yPos][xPos]=move;
-  //std::cout<<move<<" "<<xPos<<" "<<yPos<<"\n";
   for(int n=0;n<2;n++){
       jump+=1;
       yPos+=1;
       move+=1;
       graph[yPos][xPos]=move;
-      //std::cout<<move<<" "<<xPos<<" "<<yPos<<"\n";
       for(int x=0;x<jump;x++){
           xPos+=1;   
           move+=1;
           graph[yPos][xPos]=move;
-          //std::cout<<move<<" "<<xPos<<" "<<yPos<<"\n";
       }
       for(int y=0;y<jump;y++){
           yPos-=1;
           move+=1;
           graph[yPos][xPos]=move;
-          //std::cout<<move<<" "<<xPos<<" "<<yPos<<"\n";
       }
       jump+=1;
       xPos+=1;
       move+=1;
       graph[yPos][xPos]=move;
-      //std::cout<<move<<" "<<xPos<<" "<<yPos<<"\n";
       for(int y=0;y<jump;y++){
           yPos+=1;
           move+=1;
           graph[yPos][xPos]=move;
-          //std::cout<<move<<" "<<xPos<<" "<<yPos<<"\n";
       }
       for(int x=0;x<jump;x++){
           xPos-=1;
           move+=1;
           graph[yPos][xPos]=move;
-          //std::cout<<move<<" "<<xPos<<" "<<yPos<<"\n";
       }
   }
   for(int y=4;y>=0;y--){
