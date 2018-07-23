fcc11d17f2f291317d9ecd26140d9e25b8af6892
Jordan-Camilletti <jordan.camilletti@gmail.com>
2018-07-23 00:22:53 -0500

    Text files named better

--- Hour Tracker/main.cpp
+++ Hour Tracker/main.cpp
@@ -4,8 +4,9 @@
 using namespace std;
 
 int main(){
-	ifstream wage;
-	int wage=0;
+	ifstream wage,hours;
+	wage.open("C:\\Users\\Dell\\CCodes\\Hour Tracker\\Wage.txt");
+	hours.open("C:\\Users\\Dell\\CCodes\\Hour Tracker\\Hours.txt");
 	int total=0;
 	std::cout<<"(1)Check info\n(2)Change Wage\n(3)Add Hours\n(4)Remove Hours";
 	return(0);
