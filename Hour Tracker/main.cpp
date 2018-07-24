1c98a0eb195a17f3c7756e52bc7fccdde9d662a1
Jordan-Camilletti <jordan.camilletti@gmail.com>
2018-07-24 00:24:32 -0500

    Moved data from wages.txt into hours.txt

--- Hour Tracker/main.cpp
+++ Hour Tracker/main.cpp
@@ -4,13 +4,13 @@
 using namespace std;
 
 int main(){
-	ifstream wage,hours;
-	wage.open("C:\\Users\\Dell\\CCodes\\Hour Tracker\\Wage.txt");
+	ifstream wages,hours;
+	int wage,hour=0,total=0;
 	hours.open("C:\\Users\\Dell\\CCodes\\Hour Tracker\\Hours.txt");
-	int total=0;
 	std::cout<<"(1)Check info\n(2)Change Wage\n(3)Add Hours\n(4)Remove Hours";
-	while(wage>>w){
-		std::cout<<w;
-	}
+	hours>>wage;
+	std::cout<<"\n"<<wage<<"\n";
+	hours>>hour;
+	std::cout<<hour;
 	return(0);
 }
