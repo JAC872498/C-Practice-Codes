43f48580a06aaf3f29ca9d939ebd2a36e679cf01
Jordan-Camilletti <jordan.camilletti@gmail.com>
2018-07-24 22:16:40 -0500

    Input now works

--- Hour Tracker/main.cpp
+++ Hour Tracker/main.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 int main(){
 	ifstream wages,hours;
-	int wage,hour=0,total=0;
+	int wage,hour=0,total=0,choice=0;
 	hours.open("C:\\Users\\Dell\\CCodes\\Hour Tracker\\Hours.txt");
-	std::cout<<"(1)Check info\n(2)Change Wage\n(3)Add Hours\n(4)Remove Hours";
 	hours>>wage;
-	std::cout<<"\n"<<wage<<"\n";
-	hours>>hour;
-	std::cout<<hour;
+	while(choice!=5){
+		std::cout<<"(1)Check info\n(2)Change Wage\n(3)Add Hours\n(4)Remove Hours\n(5)Exit\n";
+		std::cin>>choice;
+	}
 	return(0);
 }
