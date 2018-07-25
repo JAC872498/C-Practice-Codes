94f26ebd5fe2757796a6220f9348d63226f6901e
Jordan-Camilletti <jordan.camilletti@gmail.com>
2018-07-24 22:42:08 -0500

    Hours now display better

--- Hour Tracker/main.cpp
+++ Hour Tracker/main.cpp
@@ -10,13 +10,17 @@ int main(){
 	int wage,hour,total=0,choice=0;
 	hrs.open("C:\\Users\\Dell\\CCodes\\Hour Tracker\\Hours.txt");
 	hrs>>wage;
+	while(hrs>>hour){
+		hours.push_back(hour);
+	}
 	while(choice!=5){
 		cout<<"(1)Check info\n(2)Change Wage\n(3)Add Hours\n(4)Remove Hours\n(5)Exit\n";
 		cin>>choice;
 		if(choice==1){
 			
 		}else if(choice==2){
-			
+			cout<<"\nEnter new wage.\n";
+			cin>>wage;
 		}else if(choice==3){
 			
 		}else if(choice==4){
