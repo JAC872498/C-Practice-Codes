8dea2b54a360ea4b5974e2e75194be0ee5f66115
Jordan-Camilletti <jordan.camilletti@gmail.com>
2018-07-25 22:39:29 -0500

    Total hour number now shows up

--- Hour Tracker/main.cpp
+++ Hour Tracker/main.cpp
@@ -7,6 +7,14 @@
 
 using namespace std;
 
+int hrTMnt(int hour){//Hours to minutes
+	int mins=0;
+	mins+=((hour/100)*60);
+	mins+=(hour%100);
+	return(mins);
+}
+
+
 void display_vector(const vector<int> &v)
 {
     std::copy(v.begin(), v.end(),
@@ -16,29 +24,39 @@ void display_vector(const vector<int> &v)
 int main(){
 	ifstream fls;
 	vector<int> hours,dates;
-	int wage,n,total=0,choice=0;
+	int wage,fileNum,h=0,total=0,choice=0;
 	
 	fls.open("C:\\Users\\Dell\\CCodes\\Hour Tracker\\Hours.txt");
 	fls>>wage;
-	while(fls>>n){
-		hours.push_back(n);
+	while(fls>>fileNum){
+		h+=1;
+		hours.push_back(fileNum);
+		if(h%2==0){
+			total+=hrTMnt(hours[h]);
+			total-=hrTMnt(hours[h-1]);
+		}
 	}
 	fls.close();
 	
 	fls.open("C:\\Users\\Dell\\CCodes\\Hour Tracker\\Dates.txt");
-	while(fls>>n){
-		dates.push_back(n);
+	while(fls>>fileNum){
+		dates.push_back(fileNum);
 	}
 	fls.close();
 	
-	display_vector(hours);
+	/*display_vector(hours);
 	cout<<"\n";
-	display_vector(dates);
+	display_vector(dates);*/
+	for(int t=30;t<=2400;t+=30){
+		cout<<"\n"<<hrTMnt(t);
+	}
+	
 	while(choice!=5){
-		cout<<"(1)Check info\n(2)Change Wage\n(3)Add Hours\n(4)Remove Hours\n(5)Exit\n";
+		cout<<"\n(1)Check info\n(2)Change Wage\n(3)Add Hours\n(4)Remove Hours\n(5)Exit\n";
 		cin>>choice;
 		if(choice==1){
-			
+			cout<<"Wage: $"<<(wage/100)<<"."<<(wage%100);
+			cout<<"Total Hours: ";
 		}else if(choice==2){
 			cout<<"Enter new wage.\n";
 			cin>>wage;
@@ -47,6 +65,9 @@ int main(){
 			
 		}else if(choice==4){
 			
+		}else if(choice!=5){
+			cout<<"\n(1)Check info\n(2)Change Wage\n(3)Add Hours\n(4)Remove Hours\n(5)Exit\n";
+		   	cin>>choice;
 		}
 	}
 	return(0);
