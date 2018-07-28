08c5939ab0169ad931b4b4053078bb9cb126597d
Jordan-Camilletti <jordan.camilletti@gmail.com>
2018-07-27 22:09:56 -0500

    Added values for hour input

--- Hour Tracker/main.cpp
+++ Hour Tracker/main.cpp
@@ -29,7 +29,7 @@ void display_vector(const vector<int> &v)//Temporary function to display vectors
 int main(){
 	ifstream fls;
 	vector<int> hours,timeAmounts,dates;//Hours-start and end times, timeamounts-time spent in minutes, dates-date of the hours
-	int wage,fileNum,h=0,total=0,choice=0;
+	int wage,fileNum,h=0,total=0,choice=0,startH,endH;
 	
 	fls.open("C:\\Users\\Dell\\CCodes\\Hour Tracker\\Hours.txt");
 	fls>>wage;
