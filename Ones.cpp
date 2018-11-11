3f73fec4933e791f1f755f816af6424110b0daea
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-11-10 23:52:11 -0600

    Update Ones.cpp

--- Ones.cpp
+++ Ones.cpp
@@ -2,3 +2,6 @@
 Given any integer 0≤n≤10,000 not divisible by 2 or 5, some multiple ofnis anumber which in decimal notation is a sequence of1’s
 How many digits are in thesmallest such multiple ofn?InputA file of integers at one integer per line
 */
+
+int num=0;
+std::cin>>num;
