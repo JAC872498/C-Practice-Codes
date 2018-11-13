74a6652cbe3e56eaeecdb478be33d3b06e3e4e7b
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-11-12 23:32:42 -0600

    Update A Multiplication Game.cpp

--- A Multiplication Game.cpp
+++ A Multiplication Game.cpp
@@ -1,6 +1,7 @@
 /*PG 143 of http://acm.cs.buap.mx/downloads/Programming_Challenges.pdf
-Given any integer 0≤n≤10,000 not divisible by 2 or 5, some multiple ofnis anumber which in decimal notation is a sequence of1’s
-How many digits are in thesmallest such multiple ofn?InputA file of integers at one integer per line
+Stan and Ollie play the game of multiplication by multiplying an integerpby one ofthe numbers 2 to 9. 
+Stan always starts with p=1, does his multiplication, then Olliemultiplies the number, then Stan, and so on.
+Before a game starts, they draw an integer 1<n<4,294,967,295 and the winner is whoever reachesp≥nfirst.
 */
 
 int num=0;
