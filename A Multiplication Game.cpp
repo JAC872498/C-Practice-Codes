84dfcc9afd9d0d5ae02de91b30c61e9bdb1b1312
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-11-12 23:31:53 -0600

    Update and rename Ones.cpp to A Multiplication Game.cpp

--- A Multiplication Game.cpp
+++ A Multiplication Game.cpp
@@ -1,4 +1,4 @@
-/*PG 142 of http://acm.cs.buap.mx/downloads/Programming_Challenges.pdf
+/*PG 143 of http://acm.cs.buap.mx/downloads/Programming_Challenges.pdf
 Given any integer 0≤n≤10,000 not divisible by 2 or 5, some multiple ofnis anumber which in decimal notation is a sequence of1’s
 How many digits are in thesmallest such multiple ofn?InputA file of integers at one integer per line
 */
