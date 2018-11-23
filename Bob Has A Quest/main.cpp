672e60acf6dacc87b27cfabd200a651d60c69749
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-11-22 22:59:07 -0600

    Added basics of new function

--- Bob Has A Quest/main.cpp
+++ Bob Has A Quest/main.cpp
@@ -2,6 +2,10 @@
 
 #include <iostream>
 
+String newScene(){//Used to 'clear' to screen prior to printing out a new screen
+ return(""); 
+}
+
 int main(){
   std::cout>>"Bob Has A Quest\n\n";
   return(0);
