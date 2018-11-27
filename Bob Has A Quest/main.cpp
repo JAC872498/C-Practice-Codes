38895945890b135c86bc9e133cd280f8a5dc02a5
Jordan <jordan.camilletti@gmail.com>
2018-11-26 23:33:36 -0600

    Added full clearScene() method that actually clears the scene.

--- Bob Has A Quest/main.cpp
+++ Bob Has A Quest/main.cpp
@@ -1,12 +1,24 @@
 /*A very crude and basic ASCII art game*/
 
 #include <iostream>
+#include <stdlib.h>
+#include <windows.h>
 
-std::string newScene(){//Used to 'clear' to screen prior to printing out a new screen
- return("\n\n\n"); 
+void clearScene(char fill = ' ') { 
+    COORD tl = {0,0};
+    CONSOLE_SCREEN_BUFFER_INFO s;
+    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);   
+    GetConsoleScreenBufferInfo(console, &s);
+    DWORD written, cells = s.dwSize.X * s.dwSize.Y;
+    FillConsoleOutputCharacter(console, fill, cells, tl, &written);
+    FillConsoleOutputAttribute(console, s.wAttributes, cells, tl, &written);
+    SetConsoleCursorPosition(console, tl);
 }
 
 int main(){
-  std::cout<<"Bob Has A Quest\n\n";
+  int test=0;
+  std::cout<<"Bob Has A Quest.\n\n1. He accepts the quest\n2. He rejects his fate\n";
+  std::cin>>test;
+  clearScene();
   return(0);
 }
