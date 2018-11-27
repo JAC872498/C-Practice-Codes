6b33f08b8db3fc1a76d305f7a1649edfc3a051d8
Jordan <jordan.camilletti@gmail.com>
2018-11-26 23:24:12 -0600

    Changed wording on function headers

--- Bob Has A Quest/main.cpp
+++ Bob Has A Quest/main.cpp
@@ -2,11 +2,11 @@
 
 #include <iostream>
 
-String newScene(){//Used to 'clear' to screen prior to printing out a new screen
+std::string newScene(){//Used to 'clear' to screen prior to printing out a new screen
  return("\n\n\n"); 
 }
 
 int main(){
-  std::cout>>"Bob Has A Quest\n\n";
+  std::cout<<"Bob Has A Quest\n\n";
   return(0);
 }
