28f81866ae46d3a900773a3fd8da1e85db39ad99
Jordan <jordan.camilletti@gmail.com>
2018-11-26 23:38:54 -0600

    Replaced the savage spaces with the civilized tabs

--- Bob Has A Quest/main.cpp
+++ Bob Has A Quest/main.cpp
@@ -5,20 +5,24 @@
 #include <windows.h>
 
 void clearScene(char fill = ' ') { 
-    COORD tl = {0,0};
-    CONSOLE_SCREEN_BUFFER_INFO s;
-    HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);   
-    GetConsoleScreenBufferInfo(console, &s);
-    DWORD written, cells = s.dwSize.X * s.dwSize.Y;
-    FillConsoleOutputCharacter(console, fill, cells, tl, &written);
-    FillConsoleOutputAttribute(console, s.wAttributes, cells, tl, &written);
-    SetConsoleCursorPosition(console, tl);
+	COORD tl = {0,0};
+	CONSOLE_SCREEN_BUFFER_INFO s;
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);   
+	GetConsoleScreenBufferInfo(console, &s);
+	DWORD written, cells = s.dwSize.X * s.dwSize.Y;
+	FillConsoleOutputCharacter(console, fill, cells, tl, &written);
+	FillConsoleOutputAttribute(console, s.wAttributes, cells, tl, &written);
+	SetConsoleCursorPosition(console, tl);
 }
 
 int main(){
-  int test=0;
-  std::cout<<"Bob Has A Quest.\n\n1. He accepts the quest\n2. He rejects his fate\n";
-  std::cin>>test;
-  clearScene();
-  return(0);
+	int choiceNum=0;
+	std::cout<<"Bob Has A Quest.\n\n1. He accepts the quest\n2. He rejects his fate\n";
+	std::cin>>choiceNum;
+	while(choiceNum!=1){
+		std::cout<<"NO ONE MAY REJECT THEIR FATE!\n";
+		std::cin>>choiceNum;
+	}
+	clearScene();
+	return(0);
 }
