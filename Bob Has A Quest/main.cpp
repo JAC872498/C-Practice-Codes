40a209832995fa2befe7cbeda97364fedd818c69
Jordan Camilletti <jordan.camilletti@gmail.com>
2018-11-27 23:13:27 -0600

    Adjusted some odd things in the new scene function

--- Bob Has A Quest/main.cpp
+++ Bob Has A Quest/main.cpp
@@ -4,12 +4,12 @@
 #include <stdlib.h>
 #include <windows.h>
 
-void clearScene(char fill = ' ') { 
-	COORD tl = {0,0};
+void clearScene(char fill=' ') { 
+	COORD tl={0,0};
 	CONSOLE_SCREEN_BUFFER_INFO s;
-	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);   
+	HANDLE console=GetStdHandle(STD_OUTPUT_HANDLE);   
 	GetConsoleScreenBufferInfo(console, &s);
-	DWORD written, cells = s.dwSize.X * s.dwSize.Y;
+	DWORD written, cells=s.dwSize.X*s.dwSize.Y;
 	FillConsoleOutputCharacter(console, fill, cells, tl, &written);
 	FillConsoleOutputAttribute(console, s.wAttributes, cells, tl, &written);
 	SetConsoleCursorPosition(console, tl);
