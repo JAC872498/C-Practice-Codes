a0b26e291fac3486945fd546574851a2bf9402fd
Jordan <jordan.camilletti@gmail.com>
2018-12-02 00:30:05 -0600

    All intro sentances finished.

--- Bob Has A Quest/main.cpp
+++ Bob Has A Quest/main.cpp
@@ -33,19 +33,25 @@ int main(){
 		std::cin>>storyChoice;
 	}
 	clearScene();
-	std::cout<<"So, Bob is a ";
+	std::cout<<"So, Bob is a";
 	switch(storyChoice){
 		case 1:
-			std::cout<<"peasant from 1493 from Russia.\n\n";
+			std::cout<<" peasant from #### from Russia.\n\n";
 			break;
 		case 2:
-			std::cout<<"one of the only people who can read, what a smartie.\n\n";
+			std::cout<<" one of the who can read in 1271 China, he's kinda a god due to that fact.\n\n";
 			break;
 		case 3:
+			std::cout<<" guy paid to be on a ship during ####, where is he going? Who knows?.\n\n";
 			break;
 		case 4:
+			std::cout<<"ble to do basic math, meaning he's one of the richest people in ####.\n\n";
 			break;
 		case 5:
+			std::cout<<" guy named Bob, nothing has changed.\n";
+			std::cout<<"Why did you chose this path? Why did you think this was the best choice?\n";
+			std::cout<<"Did you think it would be funny? I have to write pages about a nobody.\n";
+			std::cout<<"You could have sail the oceans, you could have watched history unfold, but no, you want to work at Burger King.\n";
 			break;
 	}
 	return(0);
