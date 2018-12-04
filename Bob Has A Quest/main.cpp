09db47d4ac488b0cfd6d29dc0915fbde750a605b
Jordan <jordan.camilletti@gmail.com>
2018-12-03 23:08:07 -0600

    Fixed years and sentance syntax

--- Bob Has A Quest/main.cpp
+++ Bob Has A Quest/main.cpp
@@ -26,26 +26,26 @@ int main(){
 	}
 	clearScene();
 	std::cout<<"Bob accepts his quest, whatever it might be.\nSpeaking of which, who is Bob?\n\n";
-	std::cout<<"1. A middle-aged peasant.\n2. A Chinese scholar.\n3. A Spanish sailor.\n4. A Moroccan merchant.\n5. A wage-slave Burger King worker.";
+	std::cout<<"1. A middle-aged peasant.\n2. A Chinese scholar.\n3. A Spanish sailor.\n4. A Moroccan merchant.\n5. A wage-slave Burger King worker.\n";
 	std::cin>>storyChoice;
 	while(storyChoice<1 || storyChoice>5){
-		std::cout<<"Sorry, there have only been 5 people named Bob in all of history.";
+		std::cout<<"Sorry, there have only been 5 people named Bob in all of history.\n";
 		std::cin>>storyChoice;
 	}
 	clearScene();
 	std::cout<<"So, Bob is a";
 	switch(storyChoice){
 		case 1:
-			std::cout<<" peasant from #### from Russia.\n\n";
+			std::cout<<" peasant from 1259 from Russia.\n";
 			break;
 		case 2:
-			std::cout<<" one of the who can read in 1271 China, he's kinda a god due to that fact.\n\n";
+			std::cout<<" one of the who can read in 1271 China, he's kinda a god due to that fact.\n";
 			break;
 		case 3:
-			std::cout<<" guy paid to be on a ship during ####, where is he going? Who knows?.\n\n";
+			std::cout<<" guy paid to be on a ship during 1513, where is he going? Who knows?.\n";
 			break;
 		case 4:
-			std::cout<<"ble to do basic math, meaning he's one of the richest people in ####.\n\n";
+			std::cout<<"ble to do basic math, meaning he's one of the richest people in 1062.\n";
 			break;
 		case 5:
 			std::cout<<" guy named Bob, nothing has changed.\n";
