33b2b84f5451c0ff7e4aa4bd0bfd10a310c45a15
Jordan <jordan.camilletti@gmail.com>
2019-01-10 23:49:32 -0600

    Main word now stored as char array instead of a clunky string

--- Hangman.cpp
+++ Hangman.cpp
@@ -1,8 +1,9 @@
 /*This is a simple hangman game that I'm making just to kill time*/
 
 #include <iostream>
+#include <bits/stdc++.h>
 
-std::string changeWord(std::string wrd, char c){
+std::string changeWord(char wrd[], char c){
 	std::string newWrd=wrd;
 	for(int n=0;n<wrd.length();n++){
 		if(wrd.split("")[n]==c){
@@ -15,10 +16,12 @@ std::string changeWord(std::string wrd, char c){
 int main(){
 	std::string man[]={   "O",
 					"\n/","|","\\",
-					  "\n/","\\"};
-	std::string word;
-	std::string guess;
+					  "\n/","\\"};	
+	std::string w;
+	char word[w.length()+1];
+	char guess;
 	int tries=0;
+	strcpy(word,w.c_str());
 	while(tries<8){
 	   	std::cout<<"Guess your letter.\n";
 		std::cin>>guess;
