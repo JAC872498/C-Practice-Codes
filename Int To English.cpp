d6364163b11853f770f72972bd2da2443052f4f8
Jordan <jordan.camilletti@gmail.com>
2019-02-04 23:45:06 -0600

    Added suffix array

--- Int To English.cpp
+++ Int To English.cpp
@@ -4,10 +4,10 @@
 #include <iostream>
 
 int main(){
-	int nums[10];
 	std::string ones[]={"one","two","three","four","five","six","seven","eight","nine"};
 	std::string tens[]={"ten","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
-	
+	std::string suffix[]={"thousand","million","billion","trillion","quadrillion"};
+	std::string wrd="";
 	int num=0;
 	std::cin>>num;
 	return(0);
