aeaccf58ca27339d6f9f7dcabed0685c787e8443
Jordan <jordan.camilletti@gmail.com>
2019-02-04 23:29:32 -0600

    Changed some syntax errors

--- Int To English.cpp
+++ Int To English.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 
 int main(){
-	int[] nums;
-	std::String[] words={"one","two","three","four","five","six","seven","eight","nine"};
+	int nums[10];
+	std::string words[]={"one","two","three","four","five","six","seven","eight","nine"};
 	int num=0;
 	std::cin>>num;
 	return(0);
