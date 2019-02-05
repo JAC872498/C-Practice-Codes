bce9c699198c42a30f9cca05d294f1ee680a037a
Jordan <jordan.camilletti@gmail.com>
2019-02-04 23:56:29 -0600

    Added useful blanks to suffix array

--- Int To English.cpp
+++ Int To English.cpp
@@ -6,7 +6,7 @@
 int main(){
 	std::string ones[]={"one","two","three","four","five","six","seven","eight","nine"};
 	std::string tens[]={"ten","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
-	std::string suffix[]={"thousand","million","billion","trillion","quadrillion"};
+	std::string suffix[]={"","thousand","million","billion","trillion","quadrillion"};
 	std::string wrd="";
 	int num=0;
 	std::cin>>num;
