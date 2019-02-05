1fe51793e4341adbe0b873ef5652639a56e7b3a0
Jordan <jordan.camilletti@gmail.com>
2019-02-04 23:31:39 -0600

    Added arrays for ones and tens

--- Int To English.cpp
+++ Int To English.cpp
@@ -5,7 +5,9 @@
 
 int main(){
 	int nums[10];
-	std::string words[]={"one","two","three","four","five","six","seven","eight","nine"};
+	std::string ones[]={"one","two","three","four","five","six","seven","eight","nine"};
+	std::string tens[]={"ten","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
+	
 	int num=0;
 	std::cin>>num;
 	return(0);
