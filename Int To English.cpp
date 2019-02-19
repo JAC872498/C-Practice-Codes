b5ac67d238a4bd26958b3acd00225ef2e1723d89
Jordan <jordan.camilletti@gmail.com>
2019-02-19 00:11:52 -0600

    Main loop now displays in string instead of numbers

--- Int To English.cpp
+++ Int To English.cpp
@@ -7,16 +7,19 @@
 #include <cmath>
 
 int main(){
-	std::string ones[]={"one","two","three","four","five","six","seven","eight","nine"};
-	std::string tens[]={"ten","twenty","thirty","fourty","fifty","sixty","seventy","eighty","ninety"};
-	std::string suffix[]={"","thousand","million","billion","trillion","quadrillion"};
+	std::string ones[]={""," one"," two"," three"," four"," five"," six"," seven"," eight"," nine"};
+	std::string tens[]={""," ten"," twenty"," thirty"," fourty"," fifty"," sixty"," seventy"," eighty"," ninety"};
+	std::string suffix[]={""," thousand"," million"," billion"," trillion"," quadrillion"};
 	std::string wrd="";
 	int num=0;
 	std::ostringstream str1; 
 	std::cin>>num;
 	str1<<num;
 	for(int n=0;n<str1.str().length();n+=3){
-		std::cout<<((int)(num/pow(10,n))%10);
+		wrd=ones[(int)(num/pow(10,n))%10]+wrd;
+		wrd=tens[(int)(num/pow(10,n+1))%10]+wrd;
+		if(ones[(int)(num/pow(10,n+2))%10]!="")
+			wrd=ones[(int)(num/pow(10,n+2))%10]+" hundred"+wrd;
 	}
 	std::cout<<wrd;
 	return(0);
