625a36ec01cae12e183e4686c194c2ab4db4e5a5
Jordan <jordan.camilletti@gmail.com>
2019-07-15 23:53:35 -0500

    Renamed enemy class

--- To Be Named/ToBeNamed.cpp
+++ To Be Named/ToBeNamed.cpp
@@ -4,7 +4,36 @@
 
 using namespace std;
 
-class test{
+class basicEnemy{
+	private:
+		int health;
+		int attack;
+		int defence;
+	int attack(){
+		
+	}
+	int defend(){
+		
+	}
+};
+
+int main(){
+	boolean turn=true;//True-player's turn, False-enemy's turn
+	int playerHealth=100;
+	int playerAttack=10;
+	int playerDefence=8;
+	while(playerHealth>0){
+		
+	}
+	return(0);
+}
+//"To Be Named" is its actual name
+//The game will be a satire about all the overdone clichés of 'fantasy' games
+#include <iostream>
+
+using namespace std;
+
+class enemy{
 	private:
 		int health;
 		int attack;
