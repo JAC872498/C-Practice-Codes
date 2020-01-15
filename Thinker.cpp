b45d353c4ec159054352ee568be6d64936b23000
Jordan <jordan.camilletti@gmail.com>
2020-01-14 23:59:05 -0600

    Added arrays for starting data

--- Thinker.cpp
+++ Thinker.cpp
@@ -5,13 +5,16 @@ Randomly creates scenarios for stories/games/etc
 #include <iostream>
 
 std::string createStartingIdea(){
-    std::string rtn="You";
-    std::string[] startingActions={" wake up","};
+    //This generates the first ideas stating who, where, when, and why
+    std::string rtn="You";//Who(expand this later?)
+    std::string startingActions={" wake up"," arrive"};
+    std::string startingAreas={""};//Where
+    std::string startingDate={""};//When
+    std::string startingReason={""};//Why
     return(rtn);
 }
 
 int main(){
-
-    std::string[] pronouns={"I","you"};
+    std::cout<<("A");
 	return(0);
 }
