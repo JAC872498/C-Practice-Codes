e90c9005a2ad2f4f20af43f28b441f96cb0f1e9d
Jordan <jordan.camilletti@gmail.com>
2020-01-14 21:05:18 -0600

    Added some basic methods

--- Thinker.cpp
+++ Thinker.cpp
@@ -2,11 +2,16 @@
 Something to make ideas for you
 Randomly creates scenarios for stories/games/etc
 */
+#include <iostream>
 
-std::String makeIdea(){
-	return("");
+std::string createStartingIdea(){
+    std::string rtn="You";
+    std::string[] startingActions={" wake up","};
+    return(rtn);
 }
 
 int main(){
+
+    std::string[] pronouns={"I","you"};
 	return(0);
 }
