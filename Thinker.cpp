eb6e886696e952d48a168886dcb20998439ab9df
Jordan <jordan.camilletti@gmail.com>
2020-01-15 23:53:49 -0600

    All starting arrays add on to rtn

--- Thinker.cpp
+++ Thinker.cpp
@@ -8,18 +8,21 @@ Randomly creates scenarios for stories/games/etc
 #include <time.h>
 
 std::string createStartingIdea(){
-    //This generates the first ideas stating who, where, when, and why
-    std::string rtn="You";//Who(expand this later?)
-    std::string startingActions[]={" wake up"," arrive"," land"};
-    std::string startingAreas[]={" within The City"," on the outskirts of the battlefield"};//Where
-    std::string startingDate[]={" on the dawn of the final day"," as the plan is just about to start"};//When
-    std::string startingReason[]={" to regain the crown"," you know what you must do"};//Why
-    rtn+=startingActions[rand()%(sizeof(startingActions)/sizeof(*startingActions))];
-    return(rtn);
+	//This generates the first ideas stating who, where, when, and why
+	std::string rtn="You";//Who(expand this later?)
+	std::string startingActions[]={" wake up"," arrive"," land"};
+	std::string startingAreas[]={" within The City"," on the outskirts of the battlefield"};//Where
+	std::string startingDate[]={" on the dawn of the final day"," as the plan is just about to start"};//When
+	std::string startingReason[]={" to regain the crown"," you know what you must do"};//Why
+	rtn+=startingActions[rand()%(sizeof(startingActions)/sizeof(*startingActions))];
+	rtn+=startingAreas[rand()%(sizeof(startingAreas)/sizeof(*startingAreas))];
+	rtn+=startingDate[rand()%(sizeof(startingDate)/sizeof(*startingDate))];
+	rtn+=startingReason[rand()%(sizeof(startingReason)/sizeof(*startingReason))];
+	return(rtn);
 }
 
 int main(){
-    srand(time(NULL));//Generates random numbers
-    std::cout<<createStartingIdea();
+	srand(time(NULL));//Generates random numbers
+	std::cout<<createStartingIdea();
 	return(0);
 }
