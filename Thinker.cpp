cf325d3ab08e5dda85f974af7a2d029555ac23d1
Jordan <jordan.camilletti@gmail.com>
2020-01-20 21:29:53 -0600

    Fixed syntax and minor spelling slip-up

--- Thinker.cpp
+++ Thinker.cpp
@@ -44,11 +44,11 @@ std::string createStartingInventory(){
 
 std::string createStartingCliffhanger(){//Maybe expand this?
 	std::string rtn="\nYou are";
-	std::string startingCliffhanger[]={" are the last of your kind, the last of the"," on your way to change history for the"};
+	std::string startingCliffhanger[]={" are the last of your kind, the last of the"," on your way to change history for "};
 	std::string startingRace[]={" the bee people"," the dragons"," the last 3 fans of Lawbreakers"," the resistance"};
 	rtn+=startingCliffhanger[rand()%(sizeof(startingCliffhanger)/sizeof(*startingCliffhanger))];
 	rtn+=startingRace[rand()%(sizeof(startingRace)/sizeof(*startingRace))];
-	rtn+="."
+	rtn+=".";
 	return(rtn);
 }
 
