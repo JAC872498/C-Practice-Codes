fa5cb38b3c4b51e8dd7c8592ea58636dfd6ba483
Jordan <jordan.camilletti@gmail.com>
2020-01-20 21:38:36 -0600

    Added comments for all arrays and methods

--- Thinker.cpp
+++ Thinker.cpp
@@ -14,8 +14,8 @@ std::string createStartingIdea(){
 	std::string startingAreas[]={" within The City"," on the outskirts of the battlefield"};//Where
 	std::string startingDate[]={". It is the dawn of the final day, it is now time",", as the plan is just about to start"};//When
 	std::string startingReason[]={" to regain the crown",", you know what you must do"};//Why
-	std::string startigFeeling[]={" scared of what's to come"," glad to finally get this over with"," like the plan shouldn't be going so smoothly"};
-	rtn+=startingActions[rand()%(sizeof(startingActions)/sizeof(*startingActions))];
+	std::string startigFeeling[]={" scared of what's to come"," glad to finally get this over with"," like the plan shouldn't be going so smoothly"};//Feelings
+	rtn+=startingActions[rand()%(sizeof(startingActions)/sizeof(*startingActions))];//This simply gets a random value of a array
 	rtn+=startingAreas[rand()%(sizeof(startingAreas)/sizeof(*startingAreas))];
 	rtn+=startingDate[rand()%(sizeof(startingDate)/sizeof(*startingDate))];
 	rtn+=startingReason[rand()%(sizeof(startingReason)/sizeof(*startingReason))];
@@ -28,10 +28,10 @@ std::string createStartingIdea(){
 std::string createStartingInventory(){
 	//This generates the main "key thing" of the story and a few other tidbits
 	std::string rtn="\nYou look within your bag to check on";
-	std::string startingKey[]={" the gold plated key"," the encrypted USB"," The One"};
-	std::string startingEnegy[]={" a few rotting apples"," your trusty blaster"," nothing, you're running out of supplies and time"};
-	std::string startingFeelingWords[]={" sad"," glad", "upset"," happy"," worried", "ecstatic"};
-	std::string startingFeelingAction[]={" that it hasn't been stolen"," that you got it this far"," that it's still asleep"};
+	std::string startingKey[]={" the gold plated key"," the encrypted USB"," The One"};//The "key"
+	std::string startingEnegy[]={" a few rotting apples"," your trusty blaster"," nothing, you're running out of supplies and time"};//Extra stuff
+	std::string startingFeelingWords[]={" sad"," glad", "upset"," happy"," worried", "ecstatic"};//Feelings
+	std::string startingFeelingAction[]={" that it hasn't been stolen"," that you got it this far"," that it's still asleep"};//Why
 	rtn+=startingKey[rand()%(sizeof(startingKey)/sizeof(*startingKey))];
 	rtn+=" along with";
 	rtn+=startingEnegy[rand()%(sizeof(startingEnegy)/sizeof(*startingEnegy))];
@@ -43,9 +43,10 @@ std::string createStartingInventory(){
 }
 
 std::string createStartingCliffhanger(){//Maybe expand this?
+	//This creates a movie-esque "cliffhanger" for the story
 	std::string rtn="\nYou are";
-	std::string startingCliffhanger[]={" are the last of your kind, the last of the"," on your way to change history for "};
-	std::string startingRace[]={" the bee people"," the dragons"," the last 3 fans of Lawbreakers"," the resistance"};
+	std::string startingCliffhanger[]={" are the last of your kind, the last of the"," on your way to change history for "};////Generic "dramatic" statement
+	std::string startingRace[]={" the bee people"," the dragons"," the last 3 fans of Lawbreakers"," the resistance"};//The group you're part of
 	rtn+=startingCliffhanger[rand()%(sizeof(startingCliffhanger)/sizeof(*startingCliffhanger))];
 	rtn+=startingRace[rand()%(sizeof(startingRace)/sizeof(*startingRace))];
 	rtn+=".";
