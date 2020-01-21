1e56896286f1152fd352587988d0e8d9158a9fb6
Jordan <jordan.camilletti@gmail.com>
2020-01-20 19:50:43 -0600

    Created 2nd method to generate an inventory

--- Thinker.cpp
+++ Thinker.cpp
@@ -13,21 +13,32 @@ std::string createStartingIdea(){
 	std::string startingActions[]={" wake up"," arrive"," land"};
 	std::string startingAreas[]={" within The City"," on the outskirts of the battlefield"};//Where
 	std::string startingDate[]={". It is the dawn of the final day, it is now time",", as the plan is just about to start"};//When
-	std::string startingReason[]={" to regain the crown.",", you know what you must do."};//Why
+	std::string startingReason[]={" to regain the crown",", you know what you must do"};//Why
+	std::string startigFeeling[]={" scared of what's to come"," glad to finally get this over with"," like the plan shouldn't be going so smoothly"};
 	rtn+=startingActions[rand()%(sizeof(startingActions)/sizeof(*startingActions))];
 	rtn+=startingAreas[rand()%(sizeof(startingAreas)/sizeof(*startingAreas))];
 	rtn+=startingDate[rand()%(sizeof(startingDate)/sizeof(*startingDate))];
 	rtn+=startingReason[rand()%(sizeof(startingReason)/sizeof(*startingReason))];
+	rtn+=", you feel";
+	rtn+=startigFeeling[rand()%(sizeof(startigFeeling)/sizeof(*startigFeeling))];
+	rtn+=".";
 	return(rtn);
 }
 
-std::string continueIdea(){
-	std::string rtn="";
+std::string createStartingInventory(){
+	std::string rtn="\nYou look within your bag to check on";
+	std::string startingKey[]={" the gold plated key"," the encrypted USB"," The One"};
+	std::string startingEnegy[]={" a few rotting apples"," your trusty blaster"," nothing, you're running out of supplies and time"};
+	rtn+=startingKey[rand()%(sizeof(startingKey)/sizeof(*startingKey))];
+	rtn+=" along with";
+	rtn+=startingEnegy[rand()%(sizeof(startingEnegy)/sizeof(*startingEnegy))];
+	rtn+=".";
 	return(rtn);
 }
 
 int main(){
 	srand(time(NULL));//Generates random numbers
 	std::cout<<createStartingIdea();
+	std::cout<<createStartingInventory();
 	return(0);
 }
