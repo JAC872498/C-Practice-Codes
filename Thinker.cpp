cacc075e5f9ef76542f1f3aaed73beab648e0136
Jordan <jordan.camilletti@gmail.com>
2020-01-30 12:23:34 -0600

    Removed TODOs and made methods be called from the object

--- Thinker.cpp
+++ Thinker.cpp
@@ -60,15 +60,14 @@ public:
 int main(){
 	/*
 	TODO:
-	Have methods create objects
 	Have a window created to put the words
-	Have image system be based on method-created objects
 	Create a series of images that can be combined
 	Create a way to combine images based on the generated stories
 	*/
 	srand(time(NULL));//Generates random numbers
-	std::cout<<createStartingIdea();
-	std::cout<<createStartingInventory();
-	std::cout<<createStartingCliffhanger();
+	Starting start;
+	std::cout<<start.createStartingIdea();
+	std::cout<<start.createStartingInventory();
+	std::cout<<start.createStartingCliffhanger();
 	return(0);
 }
